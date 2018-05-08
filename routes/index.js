e20ea9817411982778398cda5d1ed03b181c72d6
Yorick-Laptop\Yorick <yorickdevries@live.com>
2018-05-08 16:13:40 +0200

    Add new user creation

--- routes/index.js
+++ routes/index.js
@@ -26,4 +26,34 @@ router.get('/userlist', function(req, res) {
 	});
 });
 
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set our internal pool variable
+    var pool = req.pool;
+
+    // Get our form values. These rely on the "name" attributes
+    var netID = req.body.netid;
+    var email = req.body.email;
+
+    // Submit to the DB
+    pool.query('INSERT INTO usercollection(netid, email) VALUES($1, $2)', [netID, email], function (error, results, fields) {
+        if (error) {
+            // If it failed, return error
+			console.log(error);
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
 module.exports = router;
