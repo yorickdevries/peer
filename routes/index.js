70bac0a96e0aede120f0b24ef505ffa58f701654
Yorick-Laptop\Yorick <yorickdevries@live.com>
2018-05-08 15:52:24 +0200

    Add userlist page including error handling

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Homepage' });
 });
 
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+	var pool = req.pool;
+    pool.query('SELECT * FROM usercsollection', (err, result) => {
+		if(err){
+            console.log(err);
+            //res.status(400).send(err);
+			res.render('error', {
+			"message" : "Error",
+            "error" : err
+			});
+        } else {
+			//res.status(200).send(result.rows);
+			res.render('userlist', {
+            "userlist" : result.rows
+			});
+		}
+	});
+});
+
 module.exports = router;
