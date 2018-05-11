982005c4fbbdfaf871674a0f18a801eb155b382a
yorickdevries <yorickdevries@live.com>
2018-05-11 23:37:45 +0200

    Fix TSLint errors

--- src/database.ts
+++ src/database.ts
@@ -1,18 +1,18 @@
-import promise from 'bluebird';
+import promise from "bluebird";
 
-var options = {
+const options = {
   // Initialization Options
   promiseLib: promise
 };
 
-var pgp = require('pg-promise')(options);
-var connection = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'peer_database',
-    password: 'password',
+const pgp = require("pg-promise")(options);
+const connection = {
+    user: "postgres",
+    host: "localhost",
+    database: "peer_database",
+    password: "password",
     port: 5432,
   };
-var db = pgp(connection);
+const db = pgp(connection);
 
 module.exports = db;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,57 +1,57 @@
-import { Router } from 'express';
-var router = Router();
+import { Router } from "express";
+const router = Router();
 
-//import database object
-var db = require('../database');
+// import database object
+const db = require("../database");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Homepage' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Homepage" });
 });
 
 /* GET Userlist page. */
-router.get('/userlist', function(req, res) {
-    db.any('SELECT * FROM usercollection')
-    .then(function (data) {
-		//res.status(200).send(result.rows);
-		res.render('userlist', {
-            "userlist" : data
+router.get("/userlist", function (req, res) {
+    db.any("SELECT * FROM usercollection")
+        .then(function (data) {
+            // res.status(200).send(result.rows);
+            res.render("userlist", {
+                "userlist": data
             });
-    })
-    .catch(function (err) {
-        console.log(err);
-        //res.status(400).send(err);
-        res.render('error', {
-        "message" : "Error",
-        "error" : err
+        })
+        .catch(function (err) {
+            console.log(err);
+            // res.status(400).send(err);
+            res.render("error", {
+                "message": "Error",
+                "error": err
+            });
         });
-    });
 });
 
 /* GET Userlist page in JSON. */
-router.get('/userlistjson', function(req, res, next) {
-    db.any('SELECT * FROM usercollection')
-    .then(function (data) {
-      res.status(200)
-        .json(data);
-    })
-    .catch(function (err) {
-      return next(err);
-    });
+router.get("/userlistjson", function (req, res, next) {
+    db.any("SELECT * FROM usercollection")
+        .then(function (data) {
+            res.status(200)
+                .json(data);
+        })
+        .catch(function (err) {
+            return next(err);
+        });
 });
 
 /* GET New User page. */
-router.get('/newuser', function(req, res) {
-    res.render('newuser', { title: 'Add New User' });
+router.get("/newuser", function (req, res) {
+    res.render("newuser", { title: "Add New User" });
 });
 
 /* POST to Add User Service */
-router.post('/adduser', function(req, res) {
+router.post("/adduser", function (req, res) {
     // Get our form values.
-    var netID = req.body.netid;
-    var email = req.body.email;
+    const netID = req.body.netid;
+    const email = req.body.email;
 
-    db.none('INSERT INTO usercollection(netid, email) VALUES($1, $2)', [netID, email])
+    db.none("INSERT INTO usercollection(netid, email) VALUES($1, $2)", [netID, email])
         .then(function () {
             // If it worked, set the header so the address bar doesn't still say /adduser
             // And forward to success page
@@ -59,9 +59,9 @@ router.post('/adduser', function(req, res) {
         })
         .catch(function (err) {
             // If it failed, return error
-			console.log(err);
+            console.log(err);
             res.send("There was a problem adding the information to the database.");
-  });
+        });
 });
 
 export default router;
