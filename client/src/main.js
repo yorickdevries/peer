d1141d2693a41d5224f15ffbf5222050ba160850
Brian <b.planje@gmail.com>
2018-05-12 19:05:43 +0200

    Add vue-awesome as dependency for icons and registered as component

--- client/src/main.js
+++ client/src/main.js
@@ -6,6 +6,11 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/css/global.css'
+
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+
+Vue.component('icon', Icon)
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
