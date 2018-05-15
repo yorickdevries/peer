c7ce669afb662560e8fee6349f9a8de47354cc63
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2018-05-15 11:28:18 +0200

    Add several PS

--- src/prepared_statements/user_ps.ts
+++ src/prepared_statements/user_ps.ts
@@ -3,7 +3,12 @@ import pgp, {PreparedStatement} from "pg-promise";
 
 export default class UserPS {
     private db: Database;
-    private addUserQ : PreparedStatement;
+    private addUser             : PreparedStatement;
+    private getUserById         : PreparedStatement;
+    private getUserByEmail      : PreparedStatement;
+    private getCoursesIdById    : PreparedStatement;
+    private getGroupsById       : PreparedStatement;
+    private getSubmissionsById  : PreparedStatement;
 
     /**
      * Constructor for a user prepared statement (PS) object.
@@ -11,7 +16,24 @@ export default class UserPS {
      */
     constructor() {
         this.db = new Database();
-        this.addUserQ = new PreparedStatement('add-user', 'INSERT INTO "userlist" ("netid", "email") VALUES ($1, $2)');
+        this.addUser        = new PreparedStatement('add-user',
+            'INSERT INTO "userlist" ("netid", "email") VALUES ($1, $2)');
+
+        this.getUserById    = new PreparedStatement('get-user-by-id',
+            'SELECT * FROM "userlist" WHERE "netid" LIKE $1');
+
+        this.getUserByEmail = new PreparedStatement('get-user-by-email',
+            'SELECT * FROM "userlist" WHERE "email" LIKE $1');
+
+        this.getCoursesIdById  = new PreparedStatement('get-courses-by-id',
+            'SELECT * FROM "enroll" WHERE "user_netid" LIKE $1');
+
+        this.getGroupsById     = new PreparedStatement('get-groups-by-id',
+            'SELECT * FROM "groupusers" WHERE "user_netid" LIKE $1');
+
+        this.getSubmissionsById = new PreparedStatement('get-submission-by-id',
+            'SELECT * FROM "submission" WHERE "user_netid" LIKE $1');
+
 
     }
 
@@ -21,7 +43,8 @@ export default class UserPS {
      * @param {string} email - an email from the user.
      */
     public executeAddUser(netid : string, email : string) {
-        this.addUserQ.values = [netid, email];
-        this.db.executeQuery(this.addUserQ);
+        this.addUser.values = [netid, email];
+        this.db.executeQuery(this.addUser);
     }
+
 }
