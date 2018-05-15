a1b8fe5e9496c035811526fd50abb27a50896f80
PraveshMoelchand <p.p.a.moelchand@student.tudelft.nl>
2018-05-15 11:42:26 +0200

    Implement three login buttons for different roles: student, TA, teacher

---
+++ client/src/components/teaching-assistant-dashboard/TeachingAssistantDashboardLayout.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <Navbar :links="navbarItems"/>
+
+        <keep-alive><router-view></router-view></keep-alive>
+
+    </div>
+</template>
+
+<script>
+import Navbar from "../Navbar"
+
+export default {
+    name: "StudentDashboard",
+    components: {
+        Navbar
+    },
+    data() {
+        return {
+            navbarItems: [
+                { to: { name: 'StudentDashboardMain' } , text: 'Dashboard'},
+                { to: { name: 'StudentDashboardCourses' } , text: 'Courses'}
+            ]
+        }
+    }
+}
+</script>
+
+
