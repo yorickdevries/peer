01e45e09e1d95cb0a0e3f83fb2f8856b1ae42a5e
Henk Jan <j.a.h.wermelink@student.tudelft.nl>
2018-05-16 11:58:38 +0200

    fixed build failure and indentation

--- server/src/prepared_statements/group_ps.ts
+++ server/src/prepared_statements/group_ps.ts
@@ -5,11 +5,11 @@ import express = require("express");
 export default class UserPS {
     private static db: Database;
 
-    public static addGroup: PreparedStatement = new PreparedStatement("add-group", 'INSERT INTO "grouplist" ' +
-        '("group_name") VALUES ($1)');
+    public static addGroup: PreparedStatement = new PreparedStatement("add-group",
+        'INSERT INTO "grouplist" ("group_name") VALUES ($1)');
 
-    public static getGroupById: PreparedStatement = new PreparedStatement("get-group-id", 'SELECT * FROM ' +
-        '"grouplist" WHERE "id" LIKE ($1)');
+    public static getGroupById: PreparedStatement = new PreparedStatement("get-group-id",
+        'SELECT * FROM "grouplist" WHERE "id" LIKE ($1)');
 
     public static getUserOfGroupById: PreparedStatement = new PreparedStatement("get-all-users-group",
         'SELECT * FROM "groupusers" WHERE "id" LIKE ($1)');
