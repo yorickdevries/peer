b3b25b898521a0c6e4f7a7c77eadb856ff59f864
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2018-05-16 21:24:45 +0200

    Add assignment routes

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,11 +1,40 @@
 import { Router } from "express";
 import CoursesPS from "../prepared_statements/courses_ps";
+import AssignmentPS from "../prepared_statements/assignment_ps";
 import UserPS from "../prepared_statements/user_ps";
 const router = Router();
 
 // import database object
 import Database from "../database";
 import * as pgPromise from "pg-promise";
+
+
+/**
+ * Route to get all assignments for a specific course.
+ * @body course_id - course id.
+ */
+router.get("/assignments", async (req, res) => {
+    res.json(await AssignmentPS.executeGetAssignments(req.body.course_id));
+});
+
+/**
+ * Route to get an assignment for a specific course and assignment id.
+ * @body course_id - course id.
+ * @body assignment_id - assignment id to get.
+ */
+router.get("/assignment", async (req, res) => {
+    res.json(await AssignmentPS.executeAddAssignment(req.body.course_id, req.body.assignment_id));
+});
+
+/**
+ * Route to post an assignment with a title and description
+ * @body course_title - assignment title.
+ * @body course_description - assignment description.
+ */
+router.post("/assignment", async (req, res) => {
+    res.json(await AssignmentPS.executeGetAssignmentById(req.body.course_title, req.body.course_description));
+});
+
 
 /* GET Userlist page in JSON. */
 router.get("/users", function (req, res, next) {
