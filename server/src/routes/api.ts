677ebb8a1bc8d7f522be713b4a0f03d43f37bf85
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2018-05-16 21:25:53 +0200

    Cleanup

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -4,10 +4,6 @@ import AssignmentPS from "../prepared_statements/assignment_ps";
 import UserPS from "../prepared_statements/user_ps";
 const router = Router();
 
-// import database object
-import Database from "../database";
-import * as pgPromise from "pg-promise";
-
 
 /**
  * Route to get all assignments for a specific course.
@@ -33,53 +29,6 @@ router.get("/assignment", async (req, res) => {
  */
 router.post("/assignment", async (req, res) => {
     res.json(await AssignmentPS.executeGetAssignmentById(req.body.course_title, req.body.course_description));
-});
-
-
-/* GET Userlist page in JSON. */
-router.get("/users", function (req, res, next) {
-    Database.db.any("SELECT * FROM courselist")
-        .then(function (data: object) {
-            res.status(200)
-                .json(data);
-        })
-        .catch(function (err: Error) {
-            next(err);
-        });
-});
-
-/**
- * function for test purposes
- */
-
-router.post("/testquery", async function(req, res) {
-    const netId = req.body.netid;
-    const email = req.body.email;
-
-    res.json(await CoursesPS.executeCreateCourse("update", "p.j.vanderlaan-1@student.tudelft.nl"));
-});
-
-
-/* POST to Add User Service */
-router.post("/users", function (req, res) {
-    // Get our form values.
-    const netID = req.body.netid;
-    const email = req.body.email;
-
-    Database.db.none("INSERT INTO userlist(netid, email) VALUES($1, $2)", [netID, email])
-        .then(function () {
-            // If it worked, set the header so the address bar doesn't still say /adduser
-            // And forward to success page
-            res.json( { });
-        })
-        .catch(function (err: Error) {
-            // If it failed, return error
-            console.log(err);
-            res.json(
-                {
-                    error: "There was a problem adding the information to the database."
-                });
-        });
 });
 
 export default router;
