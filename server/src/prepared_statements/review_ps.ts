03108acd0faaf2e277eda2a901af85ec2768e355
Henk Jan <j.a.h.wermelink@student.tudelft.nl>
2018-05-16 21:02:32 +0200

    skeleton for reviewPS

---
+++ server/src/prepared_statements/review_ps.ts
@@ -0,0 +1,7 @@
+import Database from "../database";
+import pgp, { default as pgPromise, PreparedStatement } from "pg-promise";
+import express = require("express");
+
+export default class ReviewPS {
+
+}
