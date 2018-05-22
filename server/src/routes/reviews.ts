874e85152a5d728918f1a64bca9204add8b0c99c
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2018-05-22 19:41:15 +0200

    Add /reviews route

--- server/src/routes/reviews.ts
+++ server/src/routes/reviews.ts
@@ -1,18 +1,64 @@
-import ReviewPS from "../prepared_statements/review_ps";
+import ReviewsPS from "../prepared_statements/review_ps";
 
 // Router
 import { Router } from "express";
 const router = Router();
 
 /**
- * Route that creates a new review
- * @body comment - comment
- * @body user_netId - user_netId
- * @body submission_id - submission_id
- * @body rubric_id - rubric_id
+ * Route to get a review by review id.
+ * @param review_id - a review id.
+ * @return a database query result, all columns of review + file_path of the submission.
  */
-router.post("/peer-reviews", async (req, res) => {
-    res.json(await ReviewPS.executeCreateReview(req.body.comment, req.body.user_netid, req.body.submission_id));
+router.get("/:review_id", async (req, res) => {
+    res.json(await ReviewsPS.executeGetReview(req.params.review_id));
+});
+
+/*
+JSON (example):
+{
+    updateValues: [
+        {
+            reviewId: 1,
+            questionId: 1,
+            type: "range",
+            answer: 3
+        },
+        {
+            reviewId: 1,
+            questionId: 2,
+            type: "open",
+            answer: "very gud answer"
+        },
+        {
+            reviewId: 1,
+            questionId: 3,
+            type: "mc",
+            answer: "B"
+        }
+    ]
+}
+*/
+router.post("/", async (req, res) => {
+    req.body.updateValues.foreach(async (question: any) => {
+        switch (question.type) {
+            case "range": {
+                res.json(await ReviewsPS.executeUpdateRangeAnswer(question.answer, question.questionId, question.reviewId));
+                break;
+            }
+            case "open": {
+                res.json(await ReviewsPS.executeUpdateOpenAnswer(question.answer, question.questionId, question.reviewId));
+                break;
+            }
+            case "mc": {
+                res.json(await ReviewsPS.executeUpdateMpcAnswer(question.answer, question.questionId, question.reviewId));
+                break;
+            }
+            default: {
+                res.json({ error: "Unrecognized type given: " + question.type })
+                break;
+            }
+        }
+    });
 });
 
 export default router;
