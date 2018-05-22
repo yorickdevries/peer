eefb3028bae270d4fb540400d07f7dc54871f188
Yorick de Vries <yorickdevries@live.com>
2018-05-22 16:54:31 +0200

    Add submission routes without file upload

---
+++ server/src/prepared_statements/submissions_ps.ts
@@ -0,0 +1,48 @@
+import Database from "../database";
+import pgp, { default as pgPromise, PreparedStatement } from "pg-promise";
+
+export default class SubmissionsPS {
+    private static getSubmissions: PreparedStatement = new PreparedStatement("get-submissions",
+        'SELECT * FROM "submission"');
+
+    private static getSubmissionById: PreparedStatement = new PreparedStatement("get-submission-by-id",
+        'SELECT * FROM "submission" WHERE "id" = $1');
+
+    private static createSubmission: PreparedStatement = new PreparedStatement("create-submission",
+        'INSERT INTO "submission" ("user_netid", "assignment_id", "file_path") VALUES ($1, $2, $3) RETURNING id, user_netid, assignment_id');
+
+    private static deleteSubmission: PreparedStatement = new PreparedStatement("delete-submission",
+        'DELETE FROM "submission" WHERE "id" = $1 RETURNING id, user_netid, assignment_id');
+
+
+    /**
+     * Executes a 'get submissions'.
+     */
+    public static executeGetSubmissions(): Promise<pgPromise.queryResult> {
+        return Database.executeQuery(this.getSubmissions);
+    }
+
+    /**
+     * Executes a 'get submission by id' query
+     */
+    public static executeGetSubmissionById(id: number): Promise<pgPromise.queryResult> {
+        this.getSubmissionById.values = [id];
+        return Database.executeQuerySingleResult(this.getSubmissionById);
+    }
+
+    /**
+     * Executes a 'create submission' query
+     */
+    public static executeCreateSubmission(netid: string, assignmentId: number, filePath: string): Promise<pgPromise.queryResult> {
+        this.createSubmission.values = [netid, assignmentId, filePath];
+        return Database.executeQuerySingleResult(this.createSubmission);
+    }
+
+    /**
+     * Executes a 'delete submission' query
+     */
+    public static executeDeleteSubmissionById(submissionId: number): Promise<pgPromise.queryResult> {
+        this.deleteSubmission.values = [submissionId];
+        return Database.executeQuery(this.deleteSubmission);
+    }
+}
--- server/src/routes/submissions.ts
+++ server/src/routes/submissions.ts
@@ -1,4 +1,47 @@
+// Imports
+import SubmissionsPS from "../prepared_statements/submissions_ps";
+
 // Router
 import { Router } from "express";
 const router = Router();
+
+/**
+ * Route to get all submissions.
+ */
+router.get("/", async (req, res) => {
+    res.json(await SubmissionsPS.executeGetSubmissions());
+});
+
+/**
+ * Route to get one submission with a specific id.
+ * @param id - submission id.
+ */
+router.get("/:id", async (req, res) => {
+    res.json(await SubmissionsPS.executeGetSubmissionById(req.params.id));
+});
+
+/**
+ * Route to get one submission with a specific id.
+ * @param id - submission id.
+ */
+router.get("/:id", async (req, res) => {
+    res.json(await SubmissionsPS.executeGetSubmissionById(req.params.id));
+});
+
+/**
+ * Route to delete one submission with a specific id.
+ * @param id - submission id.
+ */
+router.delete("/:id", async (req, res) => {
+    res.json(await SubmissionsPS.executeDeleteSubmissionById(req.params.id));
+});
+
+/**
+ * Route to make a new submission.
+ */
+router.post("/", async (req: any, res) => {
+    // make path here
+    res.json(await SubmissionsPS.executeCreateSubmission(req.userinfo.given_name, req.body.assignmentId, "dummypath"));
+});
+
 export default router;
