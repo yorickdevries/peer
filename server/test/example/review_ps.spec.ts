7402f291e5d3b9ec1e6831a5e280dfc2d125dc0a
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2018-05-24 10:45:20 +0200

    Add tests

--- server/test/example/review_ps.spec.ts
+++ server/test/example/review_ps.spec.ts
@@ -5,9 +5,9 @@ import Database from "../../src/database";
 
 // load the queryfile
 import { QueryFile } from "pg-promise";
-const qf = new QueryFile("../../../database_dumps/ED3-FullDataBase.sql");
+const qf = new QueryFile("../../../database_dumps/ED3-TestDataBase.sql");
 
-describe("CoursePreparedStatement Test", () => {
+describe("ReviewPreparedStatement Test", () => {
     /**
      * Make a clean database before each test.
      */
@@ -15,14 +15,91 @@ describe("CoursePreparedStatement Test", () => {
         Database.DatabaseImport(qf).then(done);
     });
 
-    // /**
-    //  * Get all courses
-    //  */
-    // it("get all courses", async () => {
-    //     expect(await ReviewPS.executeGetAllCourses()).to.deep.equal([{
-    //         description: "This is a beautiful course description!",
-    //         id: 1,
-    //         name: "ED-3"
-    //     }]);
-    // });
+    /**
+     * Get review by id.
+     */
+    it("get all review by id", async () => {
+        expect(await ReviewPS.executeGetReview(1)).to.deep.equal([{
+            description: "This is a beautiful course description!",
+            id: 1,
+            name: "ED-3"
+        }]);
+    });
+
+    /**
+     * Submit review by id.
+     */
+    it("submit review by id", async () => {
+        expect(await ReviewPS.executeSubmitReview(1)).to.deep.equal([{
+            description: "This is a beautiful course description!",
+            id: 1,
+            name: "ED-3"
+        }]);
+    });
+
+    /**
+     * Update mc answer.
+     */
+    it("update mc answer", async () => {
+        expect(await ReviewPS.executeUpdateMpcAnswer(1, 1, 1)).to.deep.equal([{
+            description: "This is a beautiful course description!",
+            id: 1,
+            name: "ED-3"
+        }]);
+    });
+
+    /**
+     * Update open answer.
+     */
+    it("update open answer", async () => {
+        expect(await ReviewPS.executeUpdateOpenAnswer("new", 1, 1)).to.deep.equal([{
+            description: "This is a beautiful course description!",
+            id: 1,
+            name: "ED-3"
+        }]);
+    });
+
+    /**
+     * Update range answer.
+     */
+    it("update range answer", async () => {
+        expect(await ReviewPS.executeUpdateRangeAnswer(2, 1, 1)).to.deep.equal([{
+            description: "This is a beautiful course description!",
+            id: 1,
+            name: "ED-3"
+        }]);
+    });
+
+    /**
+     * Get mc answer.
+     */
+    it("get mc answer by id", async () => {
+        expect(await ReviewPS.executeGetMCAnswer(1, 1)).to.deep.equal([{
+            description: "This is a beautiful course description!",
+            id: 1,
+            name: "ED-3"
+        }]);
+    });
+
+    /**
+     * Get open answer.
+     */
+    it("get open answer by id", async () => {
+        expect(await ReviewPS.executeGetOpenAnswer(1, 1)).to.deep.equal([{
+            description: "This is a beautiful course description!",
+            id: 1,
+            name: "ED-3"
+        }]);
+    });
+
+    /**
+     * Get range answer.
+     */
+    it("get range answer by id", async () => {
+        expect(await ReviewPS.executeGetRangeAnswer(1, 1)).to.deep.equal([{
+            description: "This is a beautiful course description!",
+            id: 1,
+            name: "ED-3"
+        }]);
+    });
 });
