8f151b13e4c6b8177a7f57876f08a9eaf8c100a2
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2018-05-31 12:30:42 +0200

    Fix filter

--- client/src/components/teaching-assistant-dashboard/Course.vue
+++ client/src/components/teaching-assistant-dashboard/Course.vue
@@ -2,69 +2,64 @@
     <div>
         <b-container>
 
-            <b-row>
-                <b-col>
-                    <h1 class="mt-5"> <span class="font-weight-bold">{{ course.name }}</span> course dashboard</h1>
-                </b-col>
-            </b-row>
+            <h1 class="mt-5"> Course dashboard</h1>
 
-            <b-row>
-                <b-col>
-                    <b-card no-body>
+            <b-card>
+                <b-row>
+                    <b-col md="6" class="my-1">
+                        <b-form-group horizontal label="Filter" class="mb-0">
+                            <b-input-group>
+                                <b-form-input v-model="filter" placeholder="Type to Search" />
+                                <b-input-group-append>
+                                    <b-btn :disabled="!filter" @click="filter = ''">Clear</b-btn>
+                                </b-input-group-append>
+                            </b-input-group>
+                        </b-form-group>
+                    </b-col>
+                    <b-col md="6" class="my-1">
+                        <b-form-group horizontal label="Per page" class="mb-0">
+                            <b-form-select :options="pageOptions" v-model="perPage" />
+                        </b-form-group>
+                    </b-col>
+                </b-row>
 
-                        <b-card-body>
-                            <h4 class="card-title mb-0"> Assignments </h4>
-                        </b-card-body>
+                <b-table striped
+                         bordered
+                         show-empty
+                         stacked="md"
+                         :items=assignments
+                         :current-page="currentPage"
+                         :per-page=5
+                         :filter="filter"
+                         @filtered="onFiltered">
 
-                        <b-table striped
-                                 show-empty
-                                 stacked="md"
-                                 :items=assignments
-                                 :current-page="currentPage"
-                                 :per-page=5>
+                    <template slot="name" slot-scope="row">{{row.value.first}} {{row.value.last}}</template>
+                    <template slot="isActive" slot-scope="row">{{row.value?'Yes :)':'No :('}}</template>
+                    <template slot="actions" slot-scope="row">
+                        <!-- We use @click.stop here to prevent a 'row-clicked' event from also happening -->
+                        <b-button size="sm" @click.stop="info(row.item, row.index, $event.target)" class="mr-1">
+                            Info modal
+                        </b-button>
+                        <b-button size="sm" @click.stop="row.toggleDetails">
+                            {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
+                        </b-button>
+                    </template>
+                    <template slot="row-details" slot-scope="row">
+                        <b-card>
+                            <ul>
+                                <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value}}</li>
+                            </ul>
+                        </b-card>
+                    </template>
+                </b-table>
 
-                            <template slot="name" slot-scope="row">{{row.value.first}} {{row.value.last}}</template>
-                            <template slot="isActive" slot-scope="row">{{row.value?'Yes :)':'No :('}}</template>
-                            <template slot="actions" slot-scope="row">
-                                <!-- We use @click.stop here to prevent a 'row-clicked' event from also happening -->
-                                <b-button size="sm" @click.stop="info(row.item, row.index, $event.target)" class="mr-1">
-                                    Info modal
-                                </b-button>
-                                <b-button size="sm" @click.stop="row.toggleDetails">
-                                    {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
-                                </b-button>
-                            </template>
-                            <template slot="row-details" slot-scope="row">
-                                <b-card>
-                                    <ul>
-                                        <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value}}</li>
-                                    </ul>
-                                </b-card>
-                            </template>
-                        </b-table>
-                        
-                        <b-pagination :total-rows="assignmentsCount()" :per-page=5 v-model="currentPage" class="my-0" />
+                <b-pagination :total-rows=assignmentsCount() :per-page=5 v-model="currentPage" class="my-0" />
 
-                        <!-- Info modal -->
-                        <b-modal id="modalInfo" @hide="resetModal" :title="modalInfo.title" ok-only>
-                            <pre>{{ modalInfo.content }}</pre>
-                        </b-modal>
-                    </b-card>
-                </b-col>
-
-                <b-col cols="4">
-                    <!--Course Information-->
-                    <b-card no-body>
-                        <b-card-body>
-                            <h4 class="card-title">Course information</h4>
-                            <span class="font-weight-bold">Course</span>
-                            <p> {{course.name}} </p>
-                            <span class="font-weight-bold">Description</span>
-                            <p> {{course.description}}</p>
-                        </b-card-body>
-                    </b-card>
-                </b-col>
-            </b-row>
+                <!-- Info modal -->
+                <b-modal id="modalInfo" @hide="resetModal" :title="modalInfo.title" ok-only>
+                    <pre>{{ modalInfo.content }}</pre>
+                </b-modal>
+            </b-card>
 
         </b-container>
     </div>
@@ -98,7 +93,7 @@
                 members: ["User 1", "User 2", "User 3", "User 4", "User 5"],
                 currentPage: 1,
                 perPage: 5,
-                pageOptions: [ 5, 10, 15 ],
+                pageOptions: [ 5, 10, 15, 25, 50 ],
                 sortBy: null,
                 sortDesc: false,
                 sortDirection: 'asc',
