7d5c9e598f87a2dfea054da03157423f0ebd751d
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2018-05-31 12:08:19 +0200

    Add assignments table with pagination

--- client/src/components/teaching-assistant-dashboard/Course.vue
+++ client/src/components/teaching-assistant-dashboard/Course.vue
@@ -4,28 +4,156 @@
 
             <b-row>
                 <b-col>
-                    <h1 class="mt-5">TA Course Home</h1>
+                    <h1 class="mt-5"> <span class="font-weight-bold">{{ course.name }}</span> course dashboard</h1>
                 </b-col>
             </b-row>
 
-            
+            <b-row>
+                <b-col>
+                    <b-card no-body>
+
+                        <b-card-body>
+                            <h4 class="card-title mb-0"> Assignments </h4>
+                        </b-card-body>
+
+                        <b-table striped
+                                 show-empty
+                                 stacked="md"
+                                 :items=assignments
+                                 :current-page="currentPage"
+                                 :per-page=5>
+
+                            <template slot="name" slot-scope="row">{{row.value.first}} {{row.value.last}}</template>
+                            <template slot="isActive" slot-scope="row">{{row.value?'Yes :)':'No :('}}</template>
+                            <template slot="actions" slot-scope="row">
+                                <!-- We use @click.stop here to prevent a 'row-clicked' event from also happening -->
+                                <b-button size="sm" @click.stop="info(row.item, row.index, $event.target)" class="mr-1">
+                                    Info modal
+                                </b-button>
+                                <b-button size="sm" @click.stop="row.toggleDetails">
+                                    {{ row.detailsShowing ? 'Hide' : 'Show' }} Details
+                                </b-button>
+                            </template>
+                            <template slot="row-details" slot-scope="row">
+                                <b-card>
+                                    <ul>
+                                        <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value}}</li>
+                                    </ul>
+                                </b-card>
+                            </template>
+                        </b-table>
+                        
+                        <b-pagination :total-rows="assignmentsCount()" :per-page=5 v-model="currentPage" class="my-0" />
+
+                        <!-- Info modal -->
+                        <b-modal id="modalInfo" @hide="resetModal" :title="modalInfo.title" ok-only>
+                            <pre>{{ modalInfo.content }}</pre>
+                        </b-modal>
+                    </b-card>
+                </b-col>
+
+                <b-col cols="4">
+                    <!--Course Information-->
+                    <b-card no-body>
+                        <b-card-body>
+                            <h4 class="card-title">Course information</h4>
+                            <span class="font-weight-bold">Course</span>
+                            <p> {{course.name}} </p>
+                            <span class="font-weight-bold">Description</span>
+                            <p> {{course.description}}</p>
+                        </b-card-body>
+                    </b-card>
+                </b-col>
+            </b-row>
 
         </b-container>
     </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            // crumbs: [
-            //     {
-            //         text: 'Dashboard',
-            //         to: {name: 'teaching-assistant-dashboard.index'},
-            //         active: false
-            //     }
-            // ]
+    import api from "../../api"
+
+    export default {
+        data() {
+            return {
+                items: [
+                    {
+                        text: 'Course Home',
+                        active: true
+                    }
+                ],
+                assignments: [
+                    {
+                        title: null,
+                        description: null,
+                        due_date: null,
+                        publish_date: null,
+                        filename: null
+                    }
+                ],
+                course: {
+                    name: null,
+                    description: null
+                },
+                members: ["User 1", "User 2", "User 3", "User 4", "User 5"],
+                currentPage: 1,
+                perPage: 5,
+                pageOptions: [ 5, 10, 15 ],
+                sortBy: null,
+                sortDesc: false,
+                sortDirection: 'asc',
+                filter: null,
+                modalInfo: { title: '', content: '' }
+            }
+        },
+        computed: {
+            sortOptions () {
+                // Create an options list from our fields
+                return this.fields
+                    .filter(f => f.sortable)
+                    .map(f => { return { text: f.label, value: f.key } })
+            }
+        },
+        methods: {
+            info (item, index, button) {
+                this.modalInfo.title = `Row index: ${index}`
+                this.modalInfo.content = JSON.stringify(item, null, 2)
+                this.$root.$emit('bv::show::modal', 'modalInfo', button)
+            },
+            resetModal () {
+                this.modalInfo.title = ''
+                this.modalInfo.content = ''
+            },
+            assignmentsCount() {
+                return this.assignments.length;
+            }
+            // ,
+            // onFiltered (filteredItems) {
+            //     // Trigger pagination to update the number of buttons/pages due to filtering
+            //     this.totalRows = filteredItems.length
+            //     this.currentPage = 1
+            // }
+        },
+        async created() {
+            // Fetch course information.
+            let course = await api.getCourse(this.$route.params.id);
+            let assignments = await api.getCourseAssignments(this.$route.params.id);
+
+            // Construct an assignment array with data that will be displayed.
+            let assignmentsArray = [assignments.data.length];
+            for (let i = 0; i < assignments.data.length; i++) {
+                assignmentsArray[i] = {
+                    title: assignments.data[i].title,
+                    description: assignments.data[i].description,
+                    due_date: assignments.data[i].due_date,
+                    publish_date: assignments.data[i].publish_date,
+                    filename: assignments.data[i].filename
+                }
+            }
+
+            // Assign fetched data.
+            this.assignments = assignmentsArray;
+            this.course = course.data;
         }
     }
-}
 </script>
