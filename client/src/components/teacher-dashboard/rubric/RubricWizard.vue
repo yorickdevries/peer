8064e6c028768ef33d762af988e36064daf611d7
Brian <b.planje@gmail.com>
2018-06-01 20:27:47 +0200

    Ability to save and delete all the question types in the rubric wizard.

--- client/src/components/teacher-dashboard/rubric/RubricWizard.vue
+++ client/src/components/teacher-dashboard/rubric/RubricWizard.vue
@@ -7,17 +7,21 @@
 
                     <b-card v-for="question in rubric.questions"
                             :key="question.id"
-                            :header="`Question ${question.question_number}`"
+                            :header="`Question ${question.question_number} [${question.type_question.toUpperCase()}]`"
                             class="mb-3">
 
+                            <b-form-group label="Question Number" description="The question number. These questions will appear in this order (sorted).">
+                                <b-form-input type="number" v-model="question.question_number"/>
+                            </b-form-group>
+
                             <template v-if="question.type_question === 'open'">
-                                <b-form-group label="Question text" description="Text above the open question.">
+                                <b-form-group label="Question Text" description="Text above the open question.">
                                     <b-form-textarea v-model="question.question"/>
                                 </b-form-group>
                             </template>
 
                             <template v-if="question.type_question === 'range'">
-                                <b-form-group label="Question text" description="Text above the range question.">
+                                <b-form-group label="Question Text" description="Text above the range question.">
                                     <b-form-textarea v-model="question.question"/>
                                 </b-form-group>
                                 <b-form-group label="Range" description="Maximum stars a student can give.">
@@ -26,20 +30,21 @@
                             </template>
 
                             <template v-if="question.type_question === 'mc'">
-                                <b-form-group label="Question text" description="Text above the range question.">
+                                <b-form-group label="Question Text" description="Text above the MC question.">
                                     <b-form-textarea v-model="question.question"/>
                                 </b-form-group>
 
-                                <b-form-group label="Multiple Choice Options">
+                                <b-form-group label="Multiple Choice Options" description="Delete, edit and add MC options here. Make sure to save.">
                                     <template v-for="option in question.option">
                                         <b-form inline>
                                             <b-form-group class="mb-2">
-                                                <b-form-input v-model="option.option" class="mr-2"></b-form-input>
-                                                <b-button @click="deleteMPCOption(question, option)" variant="danger" size="sm">Delete</b-button>
+                                                <b-form-input v-model="option.option" :disabled="option.delete" class="mr-2"></b-form-input>
+                                                <b-button @click="deleteMCOption(question, option)" v-if="!option.delete" variant="danger" size="sm">Delete</b-button>
+                                                <b-button @click="undoDeleteMCOption(option)" v-else variant="seconadry" size="sm">Undo</b-button>
                                             </b-form-group>
                                         </b-form>
                                     </template>
-                                    <b-button @click="addMPCOption(question)" variant="success" size="sm">Add</b-button>
+                                    <b-button @click="addMCOption(question)" variant="success" size="sm">Add new option</b-button>
                                 </b-form-group>
                             </template>
 
@@ -62,7 +67,8 @@ import VueNotifications from 'vue-notifications'
 let apiPrefixes = {
     open: '/rubric/openquestion',
     mc: '/rubric/mcquestion',
-    range: '/rubric/rangequestion'
+    range: '/rubric/rangequestion',
+    mcoption: '/rubric/mcoption',
 }
 export default {
     data() {
@@ -85,22 +91,52 @@ export default {
             await this.fetchRubric()
         },
         async saveQuestion(question) {
-            console.log(`${apiPrefixes[question.type_question]}/${question.id}`)
-            console.log(question)
-            let res = await api.client.put(`${apiPrefixes[question.type_question]}/${question.id}`, question)
-            console.log(res)
+            // Special save function to save MC questions.
+            if (question.type_question === 'mc') return this.saveMCQuestion(question)
+
+            await api.client.put(`${apiPrefixes[question.type_question]}/${question.id}`, question)
             this.showSuccessMessage({message: 'Successfully saved question.'})
             await this.fetchRubric()
         },
-        addMPCOption(question) {
+        async saveMCQuestion(question) {
+            let options = question.option
+
+            // Save options first to the API (delete/post/put).
+            options.forEach(async option => {
+
+                if (option.delete === true)
+                    await api.client.delete(`${apiPrefixes['mcoption']}/${option.id}`)
+                else if (option.id === undefined)
+                    await api.client.post(`${apiPrefixes['mcoption']}`, option)
+                else if (option.id)
+                    await api.client.put(`${apiPrefixes['mcoption']}/${option.id}`, option)
+
+            })
+
+            // Save question text.
+            await api.client.put(`${apiPrefixes[question.type_question]}/${question.id}`, question)
+            this.showSuccessMessage({message: 'Successfully saved question.'})
+            await this.fetchRubric()
+
+        },
+        addMCOption(question) {
             question.option.push({
                 option: "",
                 mcquestion_id: question.id
             })
         },
-        deleteMPCOption(question, option) {
-            let index = question.option.findIndex(value => value === option)
-            question.option.splice(index, 1)
+        deleteMCOption(question, option) {
+            // Mark a MC option as deleted.
+            if (option.id === undefined) {
+                let index = question.option.findIndex(value => value === option)
+                question.option.splice(index, 1)
+            } else {
+                this.$set(option, 'delete', true)
+            }
+        },
+        undoDeleteMCOption(option) {
+            // Undo the mark to delete a MC option.
+            option.delete = false
         }
     },
     notifications: {
