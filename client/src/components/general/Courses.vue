ceb37e0a3fe52046641d1cc66867d55ad5f01862
Brian <b.planje@gmail.com>
2018-06-01 12:06:39 +0200

    Added (DEV) button to go to each dashboard. Blue buttons are user buttons and dynamic.

--- client/src/components/general/Courses.vue
+++ client/src/components/general/Courses.vue
@@ -5,23 +5,30 @@
             <!--Header-->
             <b-row>
                 <b-col>
-                    <h1 class="mt-5">Available Courses</h1>
-                    <b-button variant="success" class="mb-3" :to="{ name: 'teacher-dashboard.courses.create' }">Create Course</b-button>
+                    <div class="mt-5">
+                        <span class="h1">Enrolled Courses</span>
+                        <b-button variant="success" class="mb-3 float-right" :to="{ name: 'teacher-dashboard.courses.create' }">Create Course</b-button>
+                    </div>
                 </b-col>
             </b-row>
 
             <!--Course Cards-->
             <b-row>
-                <b-col cols="6" v-for="course in courses" :key="course.id">
+                <b-col cols="6" v-for="course in courses" :key="course.id" class="d-flex align-items-stretch">
                     <b-card :title="course.name" :sub-title="course.name" class="mb-3">
                         <p class="card-text">
                             {{ course.description | truncate(200)}}
                         </p>
-                        <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
-                            <b-button v-if="course.role === 'student'" variant="outline-primary" :to="{ name: 'student-dashboard.course.home', params: { courseId: course.id } }">Enter as Student</b-button>
-                            <b-button v-else-if="course.role === 'TA'" variant="outline-primary" :to="{ name: 'teaching-assistant-dashboard.course.home', params: { courseId: course.id } }">Enter as TA</b-button>
-                            <b-button v-else-if="course.role === 'teacher'" variant="outline-primary" :to="{ name: 'teacher-dashboard.course', params: { id: course.id } }">Enter as Teacher</b-button>
-                        </div>
+                        <div>
+                            <b-button-group size="sm" class="d-inline-block">
+                                <b-button v-if="course.role === 'student'" variant="outline-primary" :to="{ name: 'student-dashboard.course.home', params: { courseId: course.id } }">Enter as Student</b-button>
+                                <b-button v-else-if="course.role === 'TA'" variant="outline-primary" :to="{ name: 'teaching-assistant-dashboard.course.home', params: { courseId: course.id } }">Enter as TA</b-button>
+                                <b-button v-else-if="course.role === 'teacher'" variant="outline-primary" :to="{ name: 'teacher-dashboard.course', params: { id: course.id } }">Enter as Teacher</b-button>
+                                <b-button variant="outline-danger" :to="{ name: 'student-dashboard.course.home', params: { courseId: course.id } }">Student (DEV)</b-button>
+                                <b-button variant="outline-danger" :to="{ name: 'teaching-assistant-dashboard.course.home', params: { courseId: course.id } }">TA (DEV)</b-button>
+                                <b-button variant="outline-danger" :to="{ name: 'teacher-dashboard.course', params: { id: course.id } }">Teacher (DEV)</b-button>
+                            </b-button-group>
+                            </div>
                     </b-card>
                 </b-col>
 
