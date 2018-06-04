0a822cd510dd84813aae1b6972660237314d44e2
Brian <B.O.S.Planje@student.tudelft.nl>
2018-06-04 14:46:21 +0200

    Changed teacher assignment page to use tabs.

--- client/src/components/teacher-dashboard/assignments/Assignment.vue
+++ client/src/components/teacher-dashboard/assignments/Assignment.vue
@@ -4,51 +4,54 @@
 
             <b-row>
                 <b-col>
-                    <h1 class="mt-5">{{assignment.title}}</h1>
+                    <div class="d-flex align-items-center mt-5">
+                        <span class="h1 w-100">{{assignment.title}}</span>
+                        <b-button variant="success"
+                                  :to="{ name: 'teacher-dashboard.assignments.assignment.edit', params: {courseId: course.id, assignmentId: assignment.id} }">
+                            Edit assignment
+                        </b-button>
+                    </div>
+
                 </b-col>
             </b-row>
 
             <b-row>
                 <b-col>
                     <b-card no-body>
-                        <b-card-body>
-                            <h4 class="card-title mb-0">Details</h4>
-                        </b-card-body>
-                        <b-list-group flush>
-                            <b-list-group-item class="flex-column align-items-start">
-                                <div class="d-flex w-100 justify-content-between">
-                                    <h5 class="mb-1">Description</h5>
-                                </div>
-                                <p class="mb-1">
-                                    {{assignment.description}}
-                                </p>
-                            </b-list-group-item>
-                            <b-list-group-item class="flex-column align-items-start">
-                                <div class="d-flex w-100 justify-content-between">
-                                    <h5 class="mb-1">Publish date</h5>
-                                </div>
-                                <p class="mb-1">
-                                    {{formatDate(assignment.publish_date)}}
-                                </p>
-                            </b-list-group-item>
-                            <b-list-group-item class="flex-column align-items-start">
-                                <div class="d-flex w-100 justify-content-between">
-                                    <h5 class="mb-1">Due date</h5>
-                                </div>
-                                <p class="mb-1">
-                                    {{formatDate(assignment.due_date)}}
-                                </p>
-                            </b-list-group-item>
-                        </b-list-group>
+                        <b-tabs card>
+                            <b-tab title="Details" active>
+                                <b-list-group flush>
+                                    <b-list-group-item class="flex-column align-items-start">
+                                        <div class="d-flex w-100 justify-content-between">
+                                            <h5 class="mb-1">Description</h5>
+                                        </div>
+                                        <p class="mb-1">
+                                            {{assignment.description}}
+                                        </p>
+                                    </b-list-group-item>
+                                    <b-list-group-item class="flex-column align-items-start">
+                                        <div class="d-flex w-100 justify-content-between">
+                                            <h5 class="mb-1">Publish date</h5>
+                                        </div>
+                                        <p class="mb-1">
+                                            {{formatDate(assignment.publish_date)}}
+                                        </p>
+                                    </b-list-group-item>
+                                    <b-list-group-item class="flex-column align-items-start">
+                                        <div class="d-flex w-100 justify-content-between">
+                                            <h5 class="mb-1">Due date</h5>
+                                        </div>
+                                        <p class="mb-1">
+                                            {{formatDate(assignment.due_date)}}
+                                        </p>
+                                    </b-list-group-item>
+                                </b-list-group>
+                            </b-tab>
+                        </b-tabs>
                     </b-card>
                 </b-col>
             </b-row>
 
-            <b-row>
-                <b-col>
-                    <b-button variant="success" class="mt-3" :to="{ name: 'teacher-dashboard.assignments.assignment.edit', params: {courseId: course.id, assignmentId: assignment.id} }">Edit assignment</b-button>
-                </b-col>
-            </b-row>
 
         </b-container>
     </div>
