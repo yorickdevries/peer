2e2b99cd7b2580690de4babf2841b911f9f99709
Henk Jan <j.a.h.wermelink@student.tudelft.nl>
2018-06-07 12:29:30 +0200

    Fixed tslint erros in assignments.ts and assignmets.spec.ts

--- server/src/routes/assignments.ts
+++ server/src/routes/assignments.ts
@@ -247,7 +247,7 @@ router.get("/:id/group", async (req: any, res) => {
     const group = await UserPS.executeGetGroupsByNetIdByAssignmentId(req.userinfo.given_name, req.params.id);
     const groupId = group.group_groupid;
     const groupmembers = await GroupPS.executeGetUsersOfGroupById(groupId);
-    res.json({group, groupmembers})
+    res.json({group, groupmembers});
 });
 
 /**
@@ -255,13 +255,10 @@ router.get("/:id/group", async (req: any, res) => {
  */
 router.get("/:id/feedback", async (req: any, res) => {
     const assignmentId = req.params.id;
-    console.log('assignment: ' +assignmentId);
     const group = await UserPS.executeGetGroupsByNetIdByAssignmentId(req.userinfo.given_name, req.params.id);
     const groupId = group.group_groupid;
-    console.log('group: ' + groupId);
     const submission = await SubmissionsPS.executeGetLatestSubmissionsByAssignmentIdByGroupId(assignmentId, groupId);
     const submissionId = submission.id;
-    console.log('submission: ' + submissionId);
     res.json(await ReviewPS.executeGetReviewsByGroupIdAndAssignmentId(submissionId));
 });
 
