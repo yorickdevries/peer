a67f9bbc8e33a0bf36cd65c8167a17795f6a79a5
Brian <B.O.S.Planje@student.tudelft.nl>
2018-06-07 12:14:02 +0200

    Ability to remove TA's and change layout.

--- client/src/components/teacher-dashboard/TAManager.vue
+++ client/src/components/teacher-dashboard/TAManager.vue
@@ -6,47 +6,51 @@
             <b-row>
                 <b-col>
 
-                    <!--Table Options-->
-                    <b-row>
-                        <b-col cols="6" class="mb-3">
-                            <b-form-group horizontal label="Filter" class="mb-0 mr-4">
-                                <b-input-group>
-                                    <b-form-input v-model="filter" placeholder="Type to Search"/>
-                                    <b-input-group-append>
-                                        <b-btn :disabled="!filter" @click="filter = ''">Clear</b-btn>
-                                    </b-input-group-append>
-                                </b-input-group>
-                            </b-form-group>
-                        </b-col>
-                        <b-col cols="6">
-                            <b-form-group horizontal label="Per page">
-                                <b-form-input type="number" v-model="perPage"/>
-                            </b-form-group>
-                        </b-col>
-                    </b-row>
+                    <b-card header="Teaching Assistants">
+                        <!--Table Options-->
+                        <b-row>
+                            <b-col cols="6" class="mb-3">
+                                <b-form-group horizontal label="Filter" class="mb-0 mr-4">
+                                    <b-input-group>
+                                        <b-form-input v-model="filter" placeholder="Type to Search"/>
+                                        <b-input-group-append>
+                                            <b-btn :disabled="!filter" @click="filter = ''">Clear</b-btn>
+                                        </b-input-group-append>
+                                    </b-input-group>
+                                </b-form-group>
+                            </b-col>
+                            <b-col cols="6">
+                                <b-form-group horizontal label="Per page">
+                                    <b-form-input type="number" v-model="perPage"/>
+                                </b-form-group>
+                            </b-col>
+                        </b-row>
 
-                    <!--Table-->
-                    <b-table striped
-                             outlined
-                             show-empty
-                             stacked="md"
-                             :items=teachingAssistants
-                             :fields="fields"
-                             :current-page="currentPage"
-                             :per-page="Number(perPage)"
-                             :filter="filter">
+                        <!--Table-->
+                        <b-table striped
+                                 outlined
+                                 show-empty
+                                 stacked="md"
+                                 :items=teachingAssistants
+                                 :fields="fields"
+                                 :current-page="currentPage"
+                                 :per-page="Number(perPage)"
+                                 :filter="filter">
 
-                        <template slot="file_path" slot-scope="data">
-                            <a :href="`/api/submissions/${data.item.id}/file`"> {{data.value}} </a>
-                        </template>
-                    </b-table>
+                            <template slot="actions" slot-scope="data">
+                                <b-button @click="removeTA(data.item.user_netid)" size="sm" variant="danger">Remove
+                                </b-button>
+                            </template>
+                        </b-table>
 
-                    <!--Pagination-->
-                    <b-pagination :total-rows=this.teachingAssistants.length :per-page="Number(perPage)"
-                                  v-model="currentPage" class="my-0"/>
+                        <!--Pagination-->
+                        <b-pagination :total-rows=this.teachingAssistants.length :per-page="Number(perPage)"
+                                      v-model="currentPage" class="my-0"/>
+                    </b-card>
 
                 </b-col>
 
+                <!--Add form for TA-->
                 <b-col cols="4">
                     <b-card class="mb-3" header="Add a teaching assistant">
                         <label>NetID</label>
@@ -59,6 +63,7 @@
                         <small class="form-text text-muted">Please input a valid netID.</small>
                     </b-card>
                 </b-col>
+
             </b-row>
         </b-card>
     </b-container>
@@ -75,6 +80,7 @@
                 currentPage: 1,
                 fields: [
                     {key: 'user_netid', label: 'NetID'},
+                    'actions'
                 ],
                 perPage: 5,
                 filter: null,
@@ -98,6 +104,25 @@
                     })
                 } catch (e) {
                     return this.showErrorMessage({message: "Something went wrong adding the TA."})
+                }
+
+                // Show correct status message.
+                this.showSuccessMessage({message: `Successfully added ${this.netid} as a teaching assistant.`})
+
+                // Re-fetch teaching assistants.
+                await this.fetchTeachingAssistants()
+            },
+            async removeTA(netid) {
+                // Change the role through the API.
+                let res;
+                try {
+                    res = await api.client.put(`courses/${this.$route.params.courseId}/setRole`, {
+                        netid: netid,
+                        role: "student"
+                    })
+                } catch (e) {
+                    console.log(e)
+                    return this.showErrorMessage({message: "Something went wrong removing the TA."})
                 }
 
                 // Show correct status message.
