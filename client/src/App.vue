6435d98ed22ea682b6da58530ccaf127a77ac019
Brian <B.O.S.Planje@student.tudelft.nl>
2018-06-11 00:04:28 +0200

    Add sticky footer.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,3 +1,26 @@
 <template>
-    <router-view></router-view>
+    <div class="site">
+        <router-view class="site-content"></router-view>
+        <Footer></Footer>
+    </div>
 </template>
+
+<script>
+import Footer from './components/Footer'
+export default {
+    components: { Footer }
+}
+</script>
+
+<style>
+.site {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+}
+
+.site-content {
+    flex: 1;
+}
+
+</style>
