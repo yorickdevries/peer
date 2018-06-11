bea01897f073f66403d7b657513b1ee7aa009de9
Henk-Jan <j.a.h.wermelink@student.tudelft.nl>
2018-06-11 12:32:53 +0200

    Test running again

--- server/test/routes/assignments.spec.ts
+++ server/test/routes/assignments.spec.ts
@@ -253,7 +253,7 @@ describe("API Assignment routes", () => {
         const res = await chai.request(router).get("/1/allreviews");
         expect(res.status).to.equal(200);
         expect(res.text).to.equal(JSON.stringify(
-            [{"reviewer": "paulvanderlaan", "submitter": "henkjan"}]
+            [{"reviewer": "paulvanderlaan", "submitter": "paulvanderlaan"}]
         ));
     });
 
