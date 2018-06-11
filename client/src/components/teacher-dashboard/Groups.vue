f9087629248b0ba62d93d1c25e1a9d8f07a9581d
PraveshMoelchand <p.p.a.moelchand@student.tudelft.nl>
2018-06-11 11:50:04 +0200

    Add search bar for groups

--- client/src/components/teacher-dashboard/Groups.vue
+++ client/src/components/teacher-dashboard/Groups.vue
@@ -15,6 +15,24 @@
             </b-col>
         </b-row>
 
+        <!--Display existing groups-->
+        <b-row class="mt-3">
+            <b-col>
+
+                <b-row>
+                    <b-col cols="6" class="mb-3">
+                        <b-form-group horizontal label="Filter" class="mb-0 mr-4">
+                            <b-input-group>
+                                <b-form-input v-model="filter" placeholder="Type to search"></b-form-input>
+                                <b-input-group-append>
+                                    <b-btn :disabled="!filter" @click="filter=''">Clear</b-btn>
+                                </b-input-group-append>
+                            </b-input-group>
+                        </b-form-group>
+                    </b-col>
+                </b-row>
+            </b-col>
+        </b-row>
 
     </b-container>
 </template>
@@ -25,6 +43,11 @@
     export default {
         components: {
             ImportGroupsWizard
+        },
+        data() {
+            return {
+                filter: null
+            }
         },
         props: ['assignmentId']
     }
