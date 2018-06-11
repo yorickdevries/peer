4c4faa1e97efc0d9044de5577911c56bc1290fdf
Brian <B.O.S.Planje@student.tudelft.nl>
2018-06-11 12:51:06 +0200

    Fix bug in front-end Feedback page where async/await with loops does not work.

--- client/src/components/student-dashboard/assignment/Feedback.vue
+++ client/src/components/student-dashboard/assignment/Feedback.vue
@@ -108,11 +108,14 @@ export default {
     },
     async created() {
         // Get feedback array of reviews.
-        let metaRes = await api.getFeedbackOfAssignment(this.$route.params.assignmentId)
-        metaRes.data.forEach(async value => {
-            let review = await api.getPeerReview(value.id)
-            this.peerReviews.push(review.data)
-        })
+        let idsRes = await api.getFeedbackOfAssignment(this.$route.params.assignmentId)
+        let ids = idsRes.data.map(value => value.id)
+
+        for (let i = 0; i < ids.length; i++) {
+            let {data} = await api.getPeerReview(ids[i])
+            this.peerReviews.push(data)
+
+        }
         this.activeQuestion = this.sortedQuestionsList[0]
 
     },
