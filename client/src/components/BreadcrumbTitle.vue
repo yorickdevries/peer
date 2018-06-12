c35e5d96155b9a07ab689ca453d65423fbc081c3
Brian <B.O.S.Planje@student.tudelft.nl>
2018-06-12 13:34:26 +0200

    Add a title component with the look of a breadcrumb for all pages.

---
+++ client/src/components/BreadcrumbTitle.vue
@@ -0,0 +1,22 @@
+<template>
+    <b-breadcrumb :items="breadcrumbItems(items)"></b-breadcrumb>
+</template>
+
+<script>
+    export default {
+        props: {
+            items: Array
+        },
+        name: "Breadcrumb",
+        methods: {
+            breadcrumbItems(items) {
+                return items.map(item => {
+                    return {
+                        text: item,
+                        active: true
+                    }
+                })
+            }
+        }
+    }
+</script>
