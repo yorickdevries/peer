e3ab8cca974d559f4619fcb2c819c708ea132d0f
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2018-06-12 17:35:52 +0200

    Add several user story steps

--- server/test/routes/integration.spec.ts
+++ server/test/routes/integration.spec.ts
@@ -29,7 +29,6 @@ describe("API integration test", () => {
     afterEach(async () => {
         await TestData.removeSubmissionFiles();
         await TestData.removeAssignmentFiles();
-        await TestData.initializeAssignmentFiles();
     });
 
     /**
@@ -39,16 +38,17 @@ describe("API integration test", () => {
      * bplanje logs in as teacher:
      *  - [1] creates a course
      *  - [2] creates an assignment
-     *  - [3] imports the groups
+     *  - [3] adds an open question to the rubric
+     *  - [4] imports the groups
      *
      *  paulvanderlaan logs in as student:
-     *  - [4] submit a submission
-     *
-     *  yorickdevries logs in as student:
      *  - [5] submit a submission
      *
+     *  yorickdevries logs in as student:
+     *  - [6] submit a submission
+     *
      *  bplanje logs in as teacher:
-     *  - [6] distribute the reviews
+     *  - [7] distribute the reviews
      */
     it("Full integration test for assignments", async () => {
         // [1]
@@ -70,7 +70,6 @@ describe("API integration test", () => {
 
         // [2]
         // Create an assignment.
-        MockLogin.initialize("bplanje");
         const exampleSubmissionFile1 = path.join(__dirname, "../../example_data/assignments/assignment1.pdf");
         const assignment: any = await chai.request(router).post("/assignments/")
             .attach("assignmentFile", fs.readFileSync(exampleSubmissionFile1), "assignment1.pdf")
@@ -91,6 +90,23 @@ describe("API integration test", () => {
         console.log("Teacher created an assignment");
 
         // [3]
+        // Add an option question to the rubric
+        const openQuestion = await chai.request(router)
+            .post("/rubric/openquestion")
+            .send({ question: "opt", rubric_assignment_id: assignmentId, question_number: 1 });
+        expect(openQuestion.status).to.equal(200);
+        expect(openQuestion.text).to.equal(JSON.stringify(
+            {
+                "id": 2,
+                "question": "opt",
+                "rubric_assignment_id": assignmentId,
+                "question_number": 1,
+                "type_question": "open"
+            }
+        ));
+        console.log("Teacher created an open question");
+
+        // [4]
         // Import the groups for the created assignment.
         const file = path.join(__dirname, "../../example_data/csv_test/example_export.csv");
 
@@ -104,7 +120,7 @@ describe("API integration test", () => {
         ));
         console.log("Teacher imported the groups");
 
-        // [4]
+        // [5]
         // Login as paulvanderlaan and submit a submission.
         MockLogin.initialize("paulvanderlaan");
         const exampleSubmissionFile2 = path.join(__dirname, "../../example_data/submissions/submission1.pdf");
@@ -119,7 +135,7 @@ describe("API integration test", () => {
         expect(submissionPaulResult.assignment_id).to.equal(assignmentId);
         console.log("Paul submitted a submission");
 
-        // [5]
+        // [6]
         // Login as yorickdevries and submit a submission
         MockLogin.initialize("yorickdevries");
         const submissionYorick = await chai.request(router).post("/submissions/")
@@ -132,18 +148,64 @@ describe("API integration test", () => {
         expect(submissionYorickResult.assignment_id).to.equal(assignmentId);
         console.log("Yorick submitted a submission");
 
-        // [6]
+        // [7]
         // Distribute the reviews
         MockLogin.initialize("bplanje");
-        const res = await chai.request(router).get("/assignments/" + assignmentId + "/distributeReviews");
+        const distribution = await chai.request(router).get("/assignments/" + assignmentId + "/distributeReviews");
         // Assertions to make sure the reviews are distributed.
-        expect(res.status).to.equal(200);
-        expect(JSON.parse(res.text).length).to.equal(5);
+        expect(distribution.status).to.equal(200);
+        expect(JSON.parse(distribution.text).length).to.equal(5);
         console.log("Teacher distributed the reviews");
 
-        // [7]
+        // [8]
         // Paulvanderlaan reviews yorickdevries.
-        
+        const res = await chai.request(router)
+            .put("/")
+            .send({
+                "review": {
+                    "id": 1,
+                    "rubric_assignment_id": 1,
+                    "file_path": "submission1.pdf",
+                    "done": false
+                },
+                "form": [{
+                    "question": {
+                        "id": 1,
+                        "type_question": "mc",
+                        "question": "What is the best way to insert queries?",
+                        "question_number": 3,
+                        "option": [{"id": 1, "option": "By using pgAdmin", "mcquestion_id": 1}, {
+                            "id": 2,
+                            "option": "By using command line",
+                            "mcquestion_id": 1
+                        }, {"id": 3, "option": "By asking Brian", "mcquestion_id": 1}]
+                    }, "answer": {"answer": 1, "mcquestion_id": 1, "review_id": 1}
+                }]
+            });
+
+        expect(res.status).to.equal(200);
+        expect(res.text).to.equal(JSON.stringify({
+                "review": {
+                    "id": 1,
+                    "rubric_assignment_id": 1,
+                    "file_path": "submission1.pdf",
+                    "done": false
+                },
+                "form": [{
+                    "question": {
+                        "id": 1,
+                        "type_question": "mc",
+                        "question": "What is the best way to insert queries?",
+                        "question_number": 3,
+                        "option": [{"id": 1, "option": "By using pgAdmin", "mcquestion_id": 1}, {
+                            "id": 2,
+                            "option": "By using command line",
+                            "mcquestion_id": 1
+                        }, {"id": 3, "option": "By asking Brian", "mcquestion_id": 1}]
+                    }, "answer": {"answer": 1}
+                }]
+            }
+        ));
 
         // // [7]
         // MockLogin.initialize(router, "yorickdevries");
