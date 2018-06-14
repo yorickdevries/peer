9c3c04f70f091f995b5759a9d440e75704675168
Yorick-Laptop\Yorick <yorickdevries@live.com>
2018-06-14 15:58:46 +0200

    Change get_all_submission PS to mre robust versions

--- server/src/prepared_statements/submissions_ps.ts
+++ server/src/prepared_statements/submissions_ps.ts
@@ -31,7 +31,7 @@ export default class SubmissionsPS {
     public static executeGetAllSubmissionsByAssignmentId(id: number): Promise<pgPromise.queryResult> {
         const statement = new PreparedStatement("get-all-submissions-by-assignment-id",
         "SELECT s1.*, s2.comment_count FROM submission s1 JOIN "
-        + "(SELECT s.*, COUNT(sc.submission_id) AS comment_count FROM submission s LEFT JOIN submissioncomment sc ON s.id = sc.submission_id GROUP BY s.id) s2 ON s2.id = s1.id "
+        + "(SELECT s.id, COUNT(sc.submission_id) AS comment_count FROM submission s LEFT JOIN submissioncomment sc ON s.id = sc.submission_id GROUP BY s.id) s2 ON s2.id = s1.id "
         + "WHERE s1.assignment_id = $1");
         statement.values = [id];
         return Database.executeQuery(statement);
@@ -43,8 +43,8 @@ export default class SubmissionsPS {
     public static executeGetLatestSubmissionsByAssignmentId(id: number): Promise<pgPromise.queryResult> {
         const statement = new PreparedStatement("get-latest-submissions-by-assignment-id",
         "SELECT s1.*, s2.comment_count FROM submission s1 JOIN "
-        + "(SELECT s.*, COUNT(sc.submission_id) AS comment_count FROM submission s LEFT JOIN submissioncomment sc ON s.id = sc.submission_id GROUP BY s.id) s2 ON s2.id = s1.id "
-        + "WHERE s1.assignment_id = $1 AND s1.date = (SELECT max(date) FROM submission s3 WHERE s3.group_id = s1.group_id)"
+        + "(SELECT s.id, COUNT(sc.submission_id) AS comment_count FROM submission s LEFT JOIN submissioncomment sc ON s.id = sc.submission_id GROUP BY s.id) s2 ON s2.id = s1.id "
+        + "WHERE s1.assignment_id = $1 AND s1.date = (SELECT max(date) FROM submission s3 WHERE s3.group_id = s1.group_id AND s3.assignment_id = $1)"
     );
         statement.values = [id];
         return Database.executeQuery(statement);
