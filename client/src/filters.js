948a4528259b81bb48a28b62bdb5afbf0db5da99
PraveshMoelchand <p.p.a.moelchand@student.tudelft.nl>
2018-06-22 11:51:11 +0200

    Use NL date format (dd-mm-yyy)

--- client/src/filters.js
+++ client/src/filters.js
@@ -6,7 +6,7 @@ export default {
         Vue.filter('formatDate', (date) => {
             // Formats the date to a readable format for the UI.
             if (!(date instanceof Date)) date = new Date(date)
-            return `${date.toLocaleDateString()} ${date.getHours()}:${date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()}`
+            return `${date.toLocaleDateString('nl-NL')} ${date.getHours()}:${date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()}`
         })
     }
 }
