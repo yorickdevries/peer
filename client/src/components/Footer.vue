efebaa712be46b7d4c12af7f4ed3dc34d446579e
PraveshMoelchand <p.p.a.moelchand@student.tudelft.nl>
2019-08-12 15:16:45 +0200

    Add privacy policy page

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,6 +1,9 @@
 <template>
     <footer class="text-muted">
         <div class="container d-flex align-items-center h-100">
+            <div class="float-left ">
+                <a href="privacy">Privacy statement</a>
+            </div>
             <div class="mr-auto"></div>
             <b-img :src="require('../assets/images/tu_delft_logo_black.png')" alt="tu-delft-logo" height="30px" class="mr-5 mb-3"></b-img>
             <div class="float-right ">
