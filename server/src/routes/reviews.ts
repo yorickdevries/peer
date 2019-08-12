c3744ec841e45da66e6349d2e70ac6911d4a917e
Paulvanderlaan <P.J.vanderlaan-1@student.tudelft.nl>
2019-08-12 16:34:55 +0200

    Add multer for formdata processing

--- server/src/routes/reviews.ts
+++ server/src/routes/reviews.ts
@@ -3,6 +3,7 @@ import ReviewUpdate from "../reviewUpdate";
 import bodyParser from "body-parser";
 import index from "../security/index";
 import path from "path";
+import multer from "multer";
 import config from "../config";
 
 // Router
@@ -10,6 +11,42 @@ import express from "express";
 const router = express();
 router.use(bodyParser.json());
 
+
+// File of max 30 MB (in bytes)
+const maxSizeAssignmentFile = config.assignments.maxSizeAssignmentFile;
+const uploadReview = multer({
+    limits: {fileSize: maxSizeAssignmentFile},
+    fileFilter: function (req: any, file, callback) {
+        const ext = path.extname(file.originalname);
+        const extensions: any = config.allowed_extensions;
+        if (!(extensions.includes(ext))) {
+            req.fileValidationError = "Extension not allowed";
+            // tslint:disable-next-line
+            return callback(null, false);
+        } else {
+            // tslint:disable-next-line
+            return callback(null, true);
+        }
+    }
+}).single("assignmentFile");
+
+// File upload handling
+const uploadReviewFunction = function(req: any, res: any, next: any) {
+    uploadReview(req, res, function (err) {
+        // Error in case of too large file size
+        if (err) {
+            res.status(400);
+            res.json({ error: "File is too large" });
+        }
+        // Error in case of wrong file type
+        else if (req.fileValidationError) {
+            res.status(400);
+            res.json({ error: req.fileValidationError });
+        } else {
+            next();
+        }
+    });
+};
 
 /**
  * Route to get a review by review id.
@@ -32,7 +69,7 @@ router.route("/:reviewId").get(index.authorization.checkAuthorizationForReview, 
  * @body a json object of the whole form, as specified in the doc.
  * @return JSON representation of a review.
  */
-router.route("/:reviewId").put(index.authorization.checkReviewOwner, index.authorization.checkReviewBetweenPublishDue, async (req, res) => {
+router.route("/:reviewId").put(uploadReviewFunction, index.authorization.checkReviewOwner, index.authorization.checkReviewBetweenPublishDue, async (req, res) => {
     try {
         const reviewId = req.params.reviewId;
         const inputForm = req.body.form;
