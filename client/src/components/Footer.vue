3be9567f742158dc750a3598fe7e49d3c2033acf
PraveshMoelchand <p.p.a.moelchand@student.tudelft.nl>
2019-08-13 14:53:16 +0200

    Fix privacy policy bug

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="text-muted">
         <div class="container d-flex align-items-center h-100">
             <div class="float-left ">
-                <a href="privacy">Privacy Policy</a>
+                <router-link to="/privacy">Privacy Policy</router-link>
             </div>
             <div class="mr-auto"></div>
             <b-img :src="require('../assets/images/tu_delft_logo_black.png')" alt="tu-delft-logo" height="30px" class="mr-5 mb-3"></b-img>
