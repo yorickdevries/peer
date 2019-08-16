f47c944cd586b114ff41c25648b39bcc0f129d05
Yorick-Laptop\Yorick <yorickdevries@live.com>
2019-08-16 18:36:15 +0200

    fix assignment test

--- server/test/routes/assignments.spec.ts
+++ server/test/routes/assignments.spec.ts
@@ -538,6 +538,7 @@ describe("API Assignment routes", () => {
             new Date("2018-07-01T20:30:00Z"),
             new Date("2019-07-01T20:30:00Z"),
             new Date("2020-07-01T20:30:00Z"),
+            false,
             false);
 
         let assignmentToCopyToGroups: any = await AssignmentPS.executeGetGroupsByAssignmentId(assignmentToCopyTo.id);
