b02e3844d6aac5ac2d475bcc0b51b6372044f9ca
PraveshMoelchand <p.p.a.moelchand@student.tudelft.nl>
2019-08-16 15:49:27 +0200

    Add review done/not done filter

--- client/src/components/ta_teacher_shared/Reviews.vue
+++ client/src/components/ta_teacher_shared/Reviews.vue
@@ -1,16 +1,36 @@
 <template>
     <div>
-
+        <!--<b-row>{{allReviews}}</b-row>-->
+        <!--<b-row>{{doneReviews}}</b-row>-->
+        <!--<b-row>{{notDoneReviews}}</b-row>-->
+        <!--<b-row>{{reviews}}</b-row>-->
+        <!--{{showOnlyDoneReviews}}-->
         <!--Table Options-->
         <b-row>
             <b-col cols="6" class="mb-3">
                 <b-form-group horizontal label="Filter" class="mb-0 mr-4">
-                    <b-input-group>
+                    <b-input-group class="mb-2">
                         <b-form-input v-model="filter" placeholder="Type to Search"/>
                         <b-input-group-append>
                             <b-btn :disabled="!filter" @click="filter = ''">Clear</b-btn>
                         </b-input-group-append>
                     </b-input-group>
+
+                    <b-button-group class="mx-auto">
+                        <button @click="setShowOnlyDoneReviews(undefined)"
+                                :class="{'bg-primary': showOnlyDoneReviews === undefined, 'btn-outline-primary': showOnlyDoneReviews !== undefined, 'text-white': showOnlyDoneReviews === undefined}"
+                                class="btn btn-sm" size="sm">All reviews
+                        </button>
+                        <button @click="setShowOnlyDoneReviews(true)"
+                                :class="{'bg-primary': showOnlyDoneReviews === true, 'btn-outline-primary': showOnlyDoneReviews !== true, 'text-white': showOnlyDoneReviews === true}"
+                                class="btn btn-sm" size="sm">Done reviews
+                        </button>
+                        <button @click="setShowOnlyDoneReviews(false)"
+                                :class="{'bg-primary': showOnlyDoneReviews === false, 'btn-outline-primary': showOnlyDoneReviews !== false, 'text-white': showOnlyDoneReviews === false}"
+                                class="btn btn-sm" size="sm">Not done reviews
+                        </button>
+                    </b-button-group>
+
                 </b-form-group>
             </b-col>
             <b-col cols="6">
@@ -30,6 +50,11 @@
                  :current-page="currentPage"
                  :per-page="Number(perPage)"
                  :filter="filter">
+
+            <template slot="done" slot-scope="row">
+                <span v-if="row.item.done">Done</span>
+                <span v-if="row.item.done === false">Not done</span>
+            </template>
 
             <template slot="approved" slot-scope="row">
                 <span v-if="row.item.approved">Approved</span>
@@ -60,21 +85,51 @@
         data() {
             return {
                 reviews: [],
+                allReviews: [],
+                doneReviews: [],
+                notDoneReviews: [],
                 currentPage: 1,
                 fields: [
                     {key: 'reviewer', label: 'Reviewer'},
                     {key: 'submitter', label: 'Submitter'},
+                    {key: 'done', label: 'Status'},
                     {key: 'approved', label: 'Approval Status'},
                     {key: 'ta_netid', label: 'Graded by TA'},
                     'actions'
                 ],
                 perPage: 5,
-                filter: null
+                filter: null,
+                showOnlyDoneReviews: undefined,
             }
         },
         async created() {
-            let res = await api.getAssignmentReviews(this.assignmentId)
+            let res = await api.getAssignmentReviews(this.assignmentId, undefined)
+            this.allReviews = res.data
             this.reviews = res.data
+
+            this.processReviews()
+        },
+        methods: {
+            processReviews() {
+              for (let i = 0; i < this.allReviews.length; i++) {
+                  if (this.allReviews[i].done === true) {
+                      this.doneReviews.push(this.allReviews[i])
+                  } else {
+                      this.notDoneReviews.push(this.allReviews[i])
+                  }
+
+              }
+            },
+            async setShowOnlyDoneReviews(state) {
+                this.showOnlyDoneReviews = state
+                if (state === undefined) {
+                    this.reviews = this.allReviews
+                } else if (state === true) {
+                    this.reviews = this.doneReviews
+                } else if (state === false) {
+                    this.reviews = this.notDoneReviews
+                }
+            }
         }
     }
 </script>
