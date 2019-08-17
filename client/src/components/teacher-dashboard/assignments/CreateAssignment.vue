41b75f09f3aa7986bcb590702456bbd8a53e9130
PraveshMoelchand <p.p.a.moelchand@student.tudelft.nl>
2019-08-17 05:01:08 +0200

    I don't want to lose this code

--- client/src/components/teacher-dashboard/assignments/CreateAssignment.vue
+++ client/src/components/teacher-dashboard/assignments/CreateAssignment.vue
@@ -6,6 +6,7 @@
             <b-row>
                 <b-col>
                     <h1 class="mt-5">Create a new assignment</h1>
+                    <b-button @click="renderDate" variant="primary">Create the assignment</b-button>
                 </b-col>
             </b-row>
 
@@ -78,6 +79,7 @@
                             <!--Publish and due date of the peer review-->
                             <b-row>
                                 <b-col>
+                                    {{assignment.review_publish_day}}
                                     <b-form-group>
                                         <template slot="label">Start date and time for peer review(s)
                                             <b-badge v-b-tooltip.hover title="The date and time on which the students can start peer reviewing" variant="primary">?</b-badge>
@@ -242,10 +244,17 @@ export default {
         },
         async onSubmit() {
 
-            let pdate = new Date(this.assignment.publish_day + " " + this.assignment.publish_time).toJSON();
+            console.log(this.assignment.publish_day)
+
+            let pdate = new Date(this.assignment.publish_day.getDate() + " " + this.assignment.publish_time).toJSON();
             let ddate = new Date(this.assignment.due_day + " " + this.assignment.due_time).toJSON();
             let rpdate = new Date(this.assignment.review_publish_day + " " + this.assignment.review_publish_time).toJSON();
             let rddate = new Date(this.assignment.review_due_day + " " + this.assignment.review_due_time).toJSON();
+
+            console.log("After New Date(): " + pdate)
+            console.log("After New Date(): " + ddate)
+            console.log("After New Date(): " + rpdate)
+            console.log("After New Date(): " + rddate)
 
             this.assignment.publish_date = pdate
             this.assignment.due_date = ddate
@@ -278,8 +287,123 @@ export default {
                     this.showErrorMessage({message: e.response.data.error})
                 }
             }
+        },
 
+        renderDate() {
+            let validationResult1 = this.checkDatesEmpty()
+            if (validationResult1.error) {
+                this.showErrorMessage({message: validationResult1.error})
+            } else {
+                this.showSuccessMessage({message: "Dates are all filled in!"})
+                console.log("From datepicker: " + this.assignment.publish_day)
+                console.log("From datepicker: " + this.assignment.due_day)
+                console.log("From datepicker: " + this.assignment.review_publish_day)
+                console.log("From datepicker: " + this.assignment.review_due_day)
+                let pdate = this.assignment.publish_day
+                let ddate = this.assignment.due_day
+                let rpdate = this.assignment.review_publish_day
+                let rddate = this.assignment.review_due_day
+
+                let validationResult2 = this.checkDST()
+                if (validationResult2.error) {
+                    this.showErrorMessage({message: validationResult2.error})
+                } else {
+                    this.showSuccessMessage({message: "No DST error"})
+                    pdate.setHours(this.assignment.publish_time.substring(0,2))
+                    ddate.setHours(this.assignment.due_time.substring(0,2))
+                    rpdate.setHours(this.assignment.review_publish_time.substring(0,2))
+                    rddate.setHours(this.assignment.review_due_time.substring(0,2))
+
+                    pdate.setMinutes(this.assignment.publish_time.substring(3,5))
+                    ddate.setMinutes(this.assignment.due_time.substring(3,5))
+                    rpdate.setMinutes(this.assignment.review_publish_time.substring(3,5))
+                    rddate.setMinutes(this.assignment.review_due_time.substring(3,5))
+
+                    console.log("After setting time: " + pdate)
+                    console.log("After setting time: " + ddate)
+                    console.log("After setting time: " + rpdate)
+                    console.log("After setting time: " + rddate)
+
+                    let validationResult3 = this.checkDatesLogical()
+                    if (validationResult3.error) {
+                        this.showErrorMessage({message: validationResult3.error})
+                    } else {
+                        this.showSuccessMessage({message: "Dates are all correct!"})
+                        this.assignment.publish_date = pdate.toJSON()
+                        // console.log("After toJSON(): " + this.assignment.publish_date)
+                    }
+                }
+            }
+
+
+
+
+            // console.log(this.assignment.publish_time)
+
+            // let pdate = new Date(this.assignment.publish_day.getDate() + " " + this.assignment.publish_time).toJSON();
+            // let pdate = this.assignment.publish_day
+            // pdate.setHours(this.assignment.publish_time.substring(0,2))
+            // pdate.setMinutes(this.assignment.publish_time.substring(3,5))
+            // this.assignment.publish_date = pdate.toJSON();
+            // let ddate = new Date(this.assignment.due_day + " " + this.assignment.due_time).toJSON();
+            // let rpdate = new Date(this.assignment.review_publish_day + " " + this.assignment.review_publish_time).toJSON();
+            // let rddate = new Date(this.assignment.review_due_day + " " + this.assignment.review_due_time).toJSON();
+            //
+            // console.log("After New Date(): " + pdate)
+            // console.log(this.assignment.publish_date)
+            // console.log("After New Date(): " + ddate)
+            // console.log("After New Date(): " + rpdate)
+            // console.log("After New Date(): " + rddate)
+        },
+        checkDatesEmpty() {
+            // Check whether all dates are nonempty
+            if (this.assignment.publish_day === null) {
+                return {error: "Publish date cannot be empty!"}
+            } else if (this.assignment.due_day === null) {
+                return {error: "Hand-in date cannot be empty!"}
+            } else if (this.assignment.review_publish_day === null) {
+                return {error: "Review publish date cannot be empty!"}
+            } else if (this.assignment.review_due_day === null) {
+                return {error: "Review due date cannot be empty!"}
+            } else {
+                return true
+            }
+        },
+        checkDST() {
+
+        },
+        checkDatesLogical() {
+            // Check whether dates are logical
+            if (this.assignment.publish_date > this.assignment.due_date || this.assignment.publish_date > this.assignment.review_publish_date || this.assignment.publish_date > this.assignment.review_due_date) {
+                return {error: 'Publish date is later than other dates!'}
+            } else if (this.assignment.due_date > this.assignment.review_publish_date || this.assignment.due_date > this.assignment.review_due_date) {
+                return {error: 'Due date is later than review dates!'}
+            } else if (this.assignment.review_publish_date > this.assignment.review_due_date) {
+                return {error: 'Review start date is later than review due dates!'}
+            } else {
+                return true
+            }
         }
+        // renderDate() {
+        //     // console.log(this.assignment.publish_time)
+        //
+        //     // let pdate = new Date(this.assignment.publish_day.getDate() + " " + this.assignment.publish_time).toJSON();
+        //     let pdate = this.assignment.publish_day
+        //     // console.log(this.assignment.publish_time.substring(0,2))
+        //     console.log(this.assignment.publish_time.substring(3,5))
+        //     pdate.setHours(this.assignment.publish_time.substring(0,2))
+        //     pdate.setMinutes(this.assignment.publish_time.substring(3,5))
+        //     this.assignment.publish_date = pdate.toJSON();
+        //     let ddate = new Date(this.assignment.due_day + " " + this.assignment.due_time).toJSON();
+        //     let rpdate = new Date(this.assignment.review_publish_day + " " + this.assignment.review_publish_time).toJSON();
+        //     let rddate = new Date(this.assignment.review_due_day + " " + this.assignment.review_due_time).toJSON();
+        //
+        //     console.log("After New Date(): " + pdate)
+        //     console.log(this.assignment.publish_date)
+        //     // console.log("After New Date(): " + ddate)
+        //     // console.log("After New Date(): " + rpdate)
+        //     // console.log("After New Date(): " + rddate)
+        // }
     }
 }
 </script>
