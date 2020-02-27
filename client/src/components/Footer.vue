651b0ee50b77f7c6e5ed65c16b086c309b79e115
Brian Planje <B.O.S.Planje@student.tudelft.nl>
2020-02-27 18:13:20 +0100

    add contact link to footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -3,6 +3,7 @@
         <div class="container d-flex align-items-center h-100">
             <div class="float-left ">
                 <router-link @click.native="scrollToTop" to="/privacy">Privacy Policy</router-link>
+                <div>For questions, contact: eip-ewi@tudelft.nl</div>
             </div>
             <div class="mr-auto"></div>
             <b-img :src="require('../assets/images/tu_delft_logo_black.png')" alt="tu-delft-logo" height="30px" class="mr-5 mb-3"></b-img>
