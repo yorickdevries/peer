954a838b149aa404eb2d724b951be49119d95211
Brian Planje <b.o.s.planje@student.tudelft.nl>
2020-02-27 18:42:24 +0100

    change text in landing page

--- client/src/components/general/LandingPage.vue
+++ client/src/components/general/LandingPage.vue
@@ -8,8 +8,8 @@
                         <b-col>
                             <h1 class="display-5 font-weight-bold">Peer Review done right.</h1>
                             <p class="lead">
-                                Peer reviewing can now be done without any hassle with paper. Submit, review and manage your course
-                                all within one app!
+                                Peer reviewing can now be done without any hassle with paper. Submit, review and get feedback
+                                for your course, all within one app!
                             </p>
                         </b-col>
                     </b-row>
