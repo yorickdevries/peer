e21a64aa1c7bdc10caf284a7eea6b11adcfbf03b
Yorick <yorickdevries@live.com>
2020-05-08 13:26:15 +0200

    format tslint

--- server/test/mocha.env.ts
+++ server/test/mocha.env.ts
@@ -1,5 +1,5 @@
-if (!process.env.NODE_ENV || process.env.NODE_ENV in ['development', 'test']){
-    process.env.NODE_ENV = 'test';
+if (!process.env.NODE_ENV || process.env.NODE_ENV in ["development", "test"]) {
+    process.env.NODE_ENV = "test";
 }
 else {
     throw new Error(`NODE_ENV is set to ${process.env.NODE_ENV}`);
