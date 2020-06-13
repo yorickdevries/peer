654c6d2e4e4d051537012fa10989cdb62b12ddfa
Yorick <yorickdevries@live.com>
2020-06-13 21:21:52 +0200

    Update passport Mock library

--- server/src/middleware/login/passport_mock.ts
+++ server/src/middleware/login/passport_mock.ts
@@ -1,14 +1,17 @@
 // Mock the login of an user for development purposes
-import MockStrategy from "passport-mock-strategy";
+import MockStrategy, {
+  setupSerializeAndDeserialize,
+} from "passport-mock-strategy";
 import parseNetId from "../../util/parseNetId";
 import { PassportStatic } from "passport";
+import { UserModel } from "../../models/user";
 
 const mockPassportConfiguration = function (
   passport: PassportStatic,
   netid: string,
   affiliation: string
 ): void {
-  const userobject: any = {
+  const userObject: UserModel = {
     netid: parseNetId(netid),
     studentNumber: 1234567,
     firstName: "First",
@@ -18,28 +21,18 @@ const mockPassportConfiguration = function (
     affiliation: affiliation,
     displayName: netid,
     study: "M Computer Science",
-    organisationUnit:
+    organisationUnit: [
       "Electrical Engineering, Mathematics and Computer Science",
+    ],
   };
 
-  const strategy = new MockStrategy(
-    {
-      name: "mock",
-      user: userobject,
-    },
-    (profile: any, done: any) => {
-      return done(undefined, profile);
-    }
-  );
+  const strategy = new MockStrategy({
+    name: "mock",
+    user: userObject as any, // Added any as MockStrategy asks for a specific User type
+  });
   passport.use(strategy);
 
-  passport.serializeUser((user: any, done: any) => {
-    done(undefined, user);
-  });
-
-  passport.deserializeUser((user: any, done: any) => {
-    done(undefined, user);
-  });
+  setupSerializeAndDeserialize(passport, undefined, undefined);
 };
 
 export default mockPassportConfiguration;
