60b5a35c31b286792f9b764c5414b90b4dd7debd
Yorick <yorickdevries@live.com>
2020-06-13 15:59:52 +0200

    remove debug package

--- server/src/@types/index.d.ts
+++ server/src/@types/index.d.ts
@@ -1,6 +1,5 @@
 declare module "file-system-cache";
 declare module "forwarded-for";
-declare module "debug";
 declare module "fs-extra";
 declare module "passport-saml";
 declare module "passport-saml-metadata";
