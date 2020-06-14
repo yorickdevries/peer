56552abff81fa63048416e09893bf3d128464010
Yorick <yorickdevries@live.com>
2020-06-14 14:28:06 +0200

    Add column annotations to User

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,13 +1,34 @@
-// needs to be converted to a class
-export interface UserModel {
+import {Entity, Column, PrimaryColumn} from "typeorm";
+
+@Entity()
+export class UserModel {
+  @PrimaryColumn()
   netid: string;
+
+  @Column()
   studentNumber?: number;
+
+  @Column()
   firstName?: string;
+
+  @Column()
   prefix?: string;
+
+  @Column()
   lastName?: string;
+
+  @Column()
   email?: string;
+
+  @Column()
   affiliation?: string | string[];
+
+  @Column()
   displayName?: string;
+
+  @Column()
   study?: string;
+
+  @Column()
   organisationUnit?: string | string[];
 }
