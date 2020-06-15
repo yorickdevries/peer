8cb464b8d26dee321c54563d517e14b49e6e1e56
Yorick <yorickdevries@live.com>
2020-06-15 03:41:59 +0200

    Add Joi Schema

--- server/src/models/SSOUser.ts
+++ server/src/models/SSOUser.ts
@@ -1,6 +1,7 @@
+import Joi from "@hapi/joi";
 // The way the user is returned from TU Delft Single Sign On
 // Some fields are optional depending on whether the user is student/employee
-export interface SSOUser {
+interface SSOUser {
   netid: string;
   studentNumber?: number;
   firstName: string;
@@ -12,3 +13,19 @@ export interface SSOUser {
   study?: string | string[];
   organisationUnit?: string | string[];
 }
+
+// Joi Schema
+const SSOUserSchema = Joi.object({
+  netid: Joi.string().required(),
+  studentNumber: Joi.number(),
+  firstName: Joi.string(),
+  prefix: Joi.string(),
+  lastName: Joi.string(),
+  email: Joi.string(),
+  displayName: Joi.string(),
+  affiliation: [Joi.string(), Joi.array().items(Joi.string())],
+  study: [Joi.string(), Joi.array().items(Joi.string())],
+  organisationUnit: [Joi.string(), Joi.array().items(Joi.string())],
+});
+
+export { SSOUser, SSOUserSchema };
