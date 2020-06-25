0e369191afb4ed8b28de092c03678c89f5e05df9
Yorick <yorickdevries@live.com>
2020-06-25 22:07:29 +0200

    make sure the user is the teacher of the course

--- server/src/routes/assignments.ts
+++ server/src/routes/assignments.ts
@@ -2,7 +2,11 @@ import express from "express";
 import Joi from "@hapi/joi";
 import { validateBody } from "../middleware/validation";
 import Assignment from "../models/Assignment";
+import Course from "../models/Course";
+import UserRole from "../enum/UserRole";
+import Enrollment from "../models/Enrollment";
 import HttpStatusCode from "../enum/HttpStatusCode";
+import _ from "lodash";
 
 const router = express.Router();
 
@@ -26,25 +30,43 @@ const assignmentSchema = Joi.object({
 // needs to be checked whether the user is employee
 router.post("/", validateBody(assignmentSchema), async (req, res) => {
   try {
-    const assignment = new Assignment(
-      req.body.name,
-      req.body.courseId,
-      req.body.reviewsPerUser,
-      req.body.enrollable,
-      req.body.reviewEvaluation,
-      new Date(req.body.publishDate),
-      new Date(req.body.dueDate),
-      new Date(req.body.reviewPublishDate),
-      new Date(req.body.reviewDueDate),
-      req.body.reviewEvaluationDueDate
-        ? new Date(req.body.reviewEvaluationDueDate)
-        : null,
-      req.body.description,
-      req.body.filename,
-      req.body.externalLink
-    );
-    await assignment.save();
-    res.send(assignment);
+    const courseTeachers = await Enrollment.find({
+      where: {
+        courseId: req.body.courseId,
+        role: UserRole.TEACHER,
+      },
+    });
+    // check whether the user is teacher of the course
+    if (
+      _.some(courseTeachers, (o) => {
+        return o.userNetid === req.user!.netid;
+      })
+    ) {
+      const course = await Course.findOneOrFail(req.body.courseId);
+      const assignment = new Assignment(
+        req.body.name,
+        course,
+        req.body.reviewsPerUser,
+        req.body.enrollable,
+        req.body.reviewEvaluation,
+        new Date(req.body.publishDate),
+        new Date(req.body.dueDate),
+        new Date(req.body.reviewPublishDate),
+        new Date(req.body.reviewDueDate),
+        req.body.reviewEvaluationDueDate
+          ? new Date(req.body.reviewEvaluationDueDate)
+          : null, // when the value is null, it should not be converted to a date
+        req.body.description,
+        req.body.filename,
+        req.body.externalLink
+      );
+      await assignment.save();
+      res.send(assignment);
+    } else {
+      res
+        .status(HttpStatusCode.FORBIDDEN)
+        .send("User is not a teacher for the course");
+    }
   } catch (error) {
     res.status(HttpStatusCode.BAD_REQUEST).send(error);
   }
