a04aa9f13d1dd2bbdbaf2500d1937dcf82ad5fc7
Yorick <yorickdevries@live.com>
2020-06-26 01:29:36 +0200

    include multer upload route

--- server/src/routes/assignments.ts
+++ server/src/routes/assignments.ts
@@ -7,6 +7,8 @@ import UserRole from "../enum/UserRole";
 import Enrollment from "../models/Enrollment";
 import HttpStatusCode from "../enum/HttpStatusCode";
 import _ from "lodash";
+import upload from "../middleware/upload";
+import config from "config";
 
 const router = express.Router();
 
@@ -26,50 +28,55 @@ const assignmentSchema = Joi.object({
   externalLink: Joi.string().allow(null).required(),
 });
 // post an assignment in a course
-// needs to be checked whether the user is employee
-router.post("/", validateBody(assignmentSchema), async (req, res) => {
-  try {
-    const courseTeachers = await Enrollment.find({
-      where: {
-        courseId: req.body.courseId,
-        role: UserRole.TEACHER,
-      },
-    });
-    // check whether the user is teacher of the course
-    if (
-      _.some(courseTeachers, (o) => {
-        return o.userNetid === req.user!.netid;
-      })
-    ) {
-      // here the file needs to be saved as well
-      const course = await Course.findOneOrFail(req.body.courseId);
-      const assignment = new Assignment(
-        req.body.name,
-        course,
-        req.body.reviewsPerUser,
-        req.body.enrollable,
-        req.body.reviewEvaluation,
-        new Date(req.body.publishDate),
-        new Date(req.body.dueDate),
-        new Date(req.body.reviewPublishDate),
-        new Date(req.body.reviewDueDate),
-        req.body.reviewEvaluationDueDate
-          ? new Date(req.body.reviewEvaluationDueDate)
-          : null, // when the value is null, it should not be converted to a date
-        req.body.description,
-        null,
-        req.body.externalLink
-      );
-      await assignment.save();
-      res.send(assignment);
-    } else {
-      res
-        .status(HttpStatusCode.FORBIDDEN)
-        .send("User is not a teacher for the course");
+// allowed extensions
+const allowedExtensions = config.get("allowedExtensions") as string[];
+const maxFileSize = config.get("maxFileSize") as number;
+router.post(
+  "/",
+  upload(allowedExtensions, maxFileSize, "file"),
+  validateBody(assignmentSchema),
+  async (req, res) => {
+    try {
+      const courseTeachers = await Enrollment.find({
+        where: {
+          courseId: req.body.courseId,
+          role: UserRole.TEACHER,
+        },
+      });
+      // check whether the user is teacher of the course
+      if (
+        _.some(courseTeachers, (o) => {
+          return o.userNetid === req.user!.netid;
+        })
+      ) {
+        // here the file needs to be saved as well
+        const course = await Course.findOneOrFail(req.body.courseId);
+        const assignment = new Assignment(
+          req.body.name,
+          course,
+          req.body.reviewsPerUser,
+          req.body.enrollable,
+          req.body.reviewEvaluation,
+          req.body.publishDate,
+          req.body.dueDate,
+          req.body.reviewPublishDate,
+          req.body.reviewDueDate,
+          req.body.reviewEvaluationDueDate,
+          req.body.description,
+          null, //needs to be fixed with file object
+          req.body.externalLink
+        );
+        await assignment.save();
+        res.send(assignment);
+      } else {
+        res
+          .status(HttpStatusCode.FORBIDDEN)
+          .send("User is not a teacher for the course");
+      }
+    } catch (error) {
+      res.status(HttpStatusCode.BAD_REQUEST).send(error);
     }
-  } catch (error) {
-    res.status(HttpStatusCode.BAD_REQUEST).send(error);
   }
-});
+);
 
 export default router;
