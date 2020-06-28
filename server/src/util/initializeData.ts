5a5c744fd9e37c4376ca940f8eada65be49e4066
Yorick <yorickdevries@live.com>
2020-06-28 03:50:58 +0200

    reorganize init

--- server/src/util/initializeData.ts
+++ server/src/util/initializeData.ts
@@ -5,27 +5,34 @@ import fs from "fs";
 import path from "path";
 
 const initializeData = async function (): Promise<void> {
-  // Faculties
-  await new Faculty("ABE", "Architecture and the Built Environment").save();
-  await new Faculty("CEG", "Civil Engineering and Geosciences").save();
-  await new Faculty(
-    "EEMCS",
-    "Electrical Engineering, Mathematics & Computer Science"
-  ).save();
-  await new Faculty("IDE", "Industrial Design Engineering").save();
-  await new Faculty("AE", "Aerospace Engineering").save();
-  await new Faculty("TPM", "Technology, Policy and Management").save();
-  await new Faculty("AS", "Applied Sciences").save();
-  await new Faculty(
-    "3mE",
-    "Mechanical, Maritime and Materials Engineering"
-  ).save();
+  if (!["development", undefined].includes(process.env.NODE_ENV)) {
+    throw new Error(`NODE_ENV is set to ${process.env.NODE_ENV}`);
+  }
+
+  const faculties: [string, string][] = [
+    ["ABE", "Architecture and the Built Environment"],
+    ["CEG", "Civil Engineering and Geosciences"],
+    ["EEMCS", "Electrical Engineering, Mathematics & Computer Science"],
+    ["IDE", "Industrial Design Engineering"],
+    ["AE", "Aerospace Engineering"],
+    ["TPM", "Technology, Policy and Management"],
+    ["AS", "Applied Sciences"],
+    ["3mE", "Mechanical, Maritime and Materials Engineering"],
+  ];
+  for (const faculty of faculties) {
+    new Faculty(faculty[0], faculty[1]).save();
+  }
 
   // Academic Years
-  await new AcademicYear("2017/2018", false).save();
-  await new AcademicYear("2018/2019", false).save();
-  await new AcademicYear("2019/2020", true).save();
-  await new AcademicYear("2020/2021", true).save();
+  const academicYears: [string, boolean][] = [
+    ["2017/2018", false],
+    ["2018/2019", false],
+    ["2019/2020", true],
+    ["2020/2021", true],
+  ];
+  for (const academicYear of academicYears) {
+    new AcademicYear(academicYear[0], academicYear[1]).save();
+  }
   console.log("Initialized Faculties and Academic Years");
 
   // file folder
