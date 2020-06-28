fcba7d321243be7e50fa25a6854d788fd6d98be3
Yorick <yorickdevries@live.com>
2020-06-28 23:18:03 +0200

    add assignment to the questionnaires

--- server/src/models/ReviewQuestionnaire.ts
+++ server/src/models/ReviewQuestionnaire.ts
@@ -1,8 +1,15 @@
 import Questionnaire from "./Questionnaire";
-import { Entity } from "typeorm";
+import { Entity, OneToOne } from "typeorm";
+import Assignment from "./Assignment";
 
 @Entity()
 export default class ReviewQuestionnaire extends Questionnaire {
+  @OneToOne(
+    (_type) => Assignment,
+    (assignment) => assignment.reviewQuestionnaire
+  )
+  assignment?: Assignment;
+
   constructor() {
     super();
   }
--- server/src/models/SubmissionQuestionnaire.ts
+++ server/src/models/SubmissionQuestionnaire.ts
@@ -1,8 +1,15 @@
 import Questionnaire from "./Questionnaire";
-import { Entity } from "typeorm";
+import { Entity, OneToOne } from "typeorm";
+import Assignment from "./Assignment";
 
 @Entity()
 export default class SubmissionQuestionnaire extends Questionnaire {
+  @OneToOne(
+    (_type) => Assignment,
+    (assignment) => assignment.submissionQuestionnaire
+  )
+  assignment?: Assignment;
+
   constructor() {
     super();
   }
