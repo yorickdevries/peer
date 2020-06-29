12576e185593ff27e2bde02cbbe2f8034d3b1e4e
Yorick <yorickdevries@live.com>
2020-06-29 20:00:18 +0200

    add openquestionanswer

---
+++ server/src/models/OpenQuestionAnswer.ts
@@ -0,0 +1,20 @@
+import { ChildEntity, Column } from "typeorm";
+import QuestionAnswerType from "../enum/QuestionAnswerType";
+import QuestionAnswer from "./QuestionAnswer";
+import Review from "./Review";
+import { IsDefined, IsString, IsNotEmpty } from "class-validator";
+
+@ChildEntity(QuestionAnswerType.OPEN)
+export default class OpenQuestionAnswer extends QuestionAnswer {
+  // answer varchar(100000) NOT NULL,
+  @Column("text")
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  answer: string;
+
+  constructor(question: OpenQuestion, review: Review, answer: string) {
+    super(question, review);
+    this.answer = answer;
+  }
+}
