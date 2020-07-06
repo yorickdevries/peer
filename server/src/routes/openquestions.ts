3ba07366ce2e731b76f7cd67fcd8511ad53086ae
Yorick <yorickdevries@live.com>
2020-07-07 00:49:43 +0200

    change and delete questions

--- server/src/routes/openquestions.ts
+++ server/src/routes/openquestions.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import Joi from "@hapi/joi";
-import { validateBody } from "../middleware/validation";
+import {
+  validateParams,
+  validateBody,
+  idSchema,
+} from "../middleware/validation";
 import HttpStatusCode from "../enum/HttpStatusCode";
 import Questionnaire from "../models/Questionnaire";
 import OpenQuestion from "../models/OpenQuestion";
@@ -32,6 +36,7 @@ router.post("/", validateBody(questionSchema), async (req, res) => {
     res
       .status(HttpStatusCode.FORBIDDEN)
       .send(ResponseMessage.NOT_TEACHER_IN_COURSE);
+    return;
   }
   const assignment = await questionnaire.getAssignment();
   if (
@@ -59,6 +64,104 @@ router.post("/", validateBody(questionSchema), async (req, res) => {
     questionnaire
   );
   await question.save();
+  res.send(question);
+});
+
+// Joi inputvalidation
+const questionPatchSchema = Joi.object({
+  text: Joi.string().required(),
+  number: Joi.number().integer().required(),
+  optional: Joi.boolean().required(),
+});
+// post a question
+router.patch(
+  "/:id",
+  validateParams(idSchema),
+  validateBody(questionPatchSchema),
+  async (req, res) => {
+    const user = req.user!;
+    const questionId = req.params.id as any;
+    const question = await OpenQuestion.findOne(questionId);
+    if (!question) {
+      res
+        .status(HttpStatusCode.BAD_REQUEST)
+        .send(ResponseMessage.QUESTION_NOT_FOUND);
+      return;
+    }
+    if (!(await question.isTeacherInCourse(user))) {
+      res
+        .status(HttpStatusCode.FORBIDDEN)
+        .send(ResponseMessage.NOT_TEACHER_IN_COURSE);
+      return;
+    }
+    const questionnaire = await question.getQuestionnaire();
+    const assignment = await questionnaire.getAssignment();
+    if (
+      questionnaire instanceof SubmissionQuestionnaire &&
+      assignment.isAtOrAfterState(AssignmentState.REVIEW)
+    ) {
+      res
+        .status(HttpStatusCode.FORBIDDEN)
+        .send("The assignment is already in review state");
+      return;
+    }
+    if (
+      questionnaire instanceof ReviewQuestionnaire &&
+      assignment.isAtOrAfterState(AssignmentState.FEEDBACK)
+    ) {
+      res
+        .status(HttpStatusCode.FORBIDDEN)
+        .send("The assignment is already in feedback state");
+      return;
+    }
+    // otherwise update the question
+    question.text = req.body.text;
+    question.number = req.body.number;
+    question.optional = req.body.optional;
+    await question.save();
+    res.send(question);
+  }
+);
+
+// post a question
+router.delete("/:id", validateParams(idSchema), async (req, res) => {
+  const user = req.user!;
+  const questionId = req.params.id as any;
+  const question = await OpenQuestion.findOne(questionId);
+  if (!question) {
+    res
+      .status(HttpStatusCode.BAD_REQUEST)
+      .send(ResponseMessage.QUESTION_NOT_FOUND);
+    return;
+  }
+  if (!(await question.isTeacherInCourse(user))) {
+    res
+      .status(HttpStatusCode.FORBIDDEN)
+      .send(ResponseMessage.NOT_TEACHER_IN_COURSE);
+    return;
+  }
+  const questionnaire = await question.getQuestionnaire();
+  const assignment = await questionnaire.getAssignment();
+  if (
+    questionnaire instanceof SubmissionQuestionnaire &&
+    assignment.isAtOrAfterState(AssignmentState.REVIEW)
+  ) {
+    res
+      .status(HttpStatusCode.FORBIDDEN)
+      .send("The assignment is already in review state");
+    return;
+  }
+  if (
+    questionnaire instanceof ReviewQuestionnaire &&
+    assignment.isAtOrAfterState(AssignmentState.FEEDBACK)
+  ) {
+    res
+      .status(HttpStatusCode.FORBIDDEN)
+      .send("The assignment is already in feedback state");
+    return;
+  }
+  // otherwise update the question
+  await question.remove();
   res.send(question);
 });
 
