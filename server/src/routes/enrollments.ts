43982a76d3657538af6d49ebd2fe4d9e4c65c2f8
Yorick <yorickdevries@live.com>
2020-07-06 21:29:34 +0200

    create an enrollment

--- server/src/routes/enrollments.ts
+++ server/src/routes/enrollments.ts
@@ -4,8 +4,10 @@ import HttpStatusCode from "../enum/HttpStatusCode";
 import Course from "../models/Course";
 import UserRole from "../enum/UserRole";
 import Joi from "@hapi/joi";
-import { validateQuery } from "../middleware/validation";
+import { validateQuery, validateBody } from "../middleware/validation";
 import ResponseMessage from "../enum/ResponseMessage";
+import { getManager } from "typeorm";
+import User from "../models/User";
 
 const router = express.Router();
 
@@ -38,6 +40,61 @@ router.get("/", validateQuery(queryCourseIdSchema), async (req, res) => {
     order: { userNetid: "ASC" },
   });
   res.send(enrollments);
+});
+
+// Joi inputvalidation
+const enrollmentSchema = Joi.object({
+  courseId: Joi.number().integer().required(),
+  userNetid: Joi.string().required(),
+  role: Joi.string().valid(...Object.values(UserRole)),
+});
+// create an enrollment for a course
+router.post("/", validateBody(enrollmentSchema), async (req, res) => {
+  const user = req.user!;
+  // this value has been parsed by the validate function
+  const userNetid = req.body.userNetid as any;
+  const role = req.body.role as any;
+  const courseId = req.body.courseId as any;
+  const course = await Course.findOne(courseId);
+  if (!course) {
+    res
+      .status(HttpStatusCode.BAD_REQUEST)
+      .send(ResponseMessage.COURSE_NOT_FOUND);
+    return;
+  }
+  if (!(await course.isEnrolled(user, UserRole.TEACHER))) {
+    res
+      .status(HttpStatusCode.FORBIDDEN)
+      .send(ResponseMessage.NOT_TEACHER_IN_COURSE);
+    return;
+  }
+  let enrollment: Enrollment;
+  await getManager().transaction(
+    "SERIALIZABLE",
+    async (transactionalEntityManager) => {
+      let user = await transactionalEntityManager.findOne(User, userNetid);
+      // in case the user doesnt exists in the database yet, create it
+      if (!user) {
+        user = new User(userNetid);
+        await transactionalEntityManager.save(user);
+      }
+      // enroll user in the course if not already
+      const existingEnrollment = await transactionalEntityManager.findOne(
+        Enrollment,
+        {
+          where: { userNetid: user.netid, courseId: course.id },
+        }
+      );
+      if (existingEnrollment) {
+        throw new Error("User is already enrolled in this course");
+      }
+      // enroll the user as student in the course
+      enrollment = new Enrollment(user, course, role);
+      await transactionalEntityManager.save(enrollment);
+    }
+  );
+  await enrollment!.reload();
+  res.send(enrollment!);
 });
 
 // get all the enrollments of the current user
