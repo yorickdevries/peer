52bf514aba9998ae89688b685833a2bbcb64eff8
Yorick <yorickdevries@live.com>
2020-07-08 23:25:01 +0200

    add route to export the reviews

---
+++ server/src/util/parseReviewsForExport.ts
@@ -0,0 +1,116 @@
+import SubmissionQuestionnaire from "../models/SubmissionQuestionnaire";
+import _ from "lodash";
+import ReviewOfSubmission from "../models/ReviewOfSubmission";
+import Question from "../models/Question";
+
+// TODO Fields like savedAt, StartedAt etc are not set yet
+// These should be tested with the frontend to be sure they are triggered properly
+
+const parseSubmissionReviewsForExport = async function (
+  submissionQuestionnaire: SubmissionQuestionnaire
+): Promise<any[]> {
+  // iterate over all reviews and save the data
+  const questions = _.sortBy(submissionQuestionnaire.questions, (question) => {
+    return question.number;
+  });
+  const reviews = (await submissionQuestionnaire.getReviews()) as ReviewOfSubmission[];
+  const assignment = await submissionQuestionnaire.getAssignment();
+
+  const parsedReviews: any[] = [];
+  for (const review of reviews) {
+    const parsedReview: any = {};
+    const submission = review.submission!;
+
+    const submitter = await submission.getUser();
+    const submitterGroup = await submission.getGroup();
+
+    const reviewer = review.reviewer;
+    const reviewerGroup = await assignment.getGroup(reviewer);
+
+    // id
+    parsedReview["id"] = review.id;
+    // Submitter netid
+    parsedReview["Submitter netid"] = submitter.netid;
+    // Submitter studentnumber
+    parsedReview["Submitter studentnumber"] = submitter.studentNumber;
+    // Submitter group id
+    parsedReview["Submitter group id"] = submitterGroup.id;
+    // Submitter group name
+    parsedReview["Submitter group name"] = submitterGroup.name;
+    // Reviewer netid
+    parsedReview["Reviewer netid"] = reviewer.netid;
+    // Reviewer studentnumber
+    parsedReview["Reviewer studentnumber"] = reviewer.studentNumber;
+    // Reviewer group id
+    parsedReview["Reviewer group id"] = reviewerGroup?.id;
+    // Reviewer group name
+    parsedReview["Reviewer group name"] = reviewerGroup?.name;
+    // Submission review started_at
+    parsedReview["Submission review started_at"] = review.startedAt;
+    // Submission review downloaded_at
+    parsedReview["Submission review downloaded_at"] = review.downloadedAt;
+    // Submission review saved_at
+    parsedReview["Submission review saved_at"] = review.savedAt;
+    // Submission review submitted_at
+    parsedReview["Submission review submitted_at"] = review.submittedAt;
+    // Submission review done
+    parsedReview["Submission review done"] = review.submitted;
+    // Approval status
+    parsedReview["Approval status"] = review.approvalByTA;
+    // TA netid
+    parsedReview["TA netid"] = review.approvingTA?.netid;
+    // Reviewer reported the submission
+    parsedReview["Reviewer reported the submission"] = review.flaggedByReviewer;
+
+    // iterate over all questions
+    for (const question of questions) {
+      const questionText = `R${question.number}. ${question.text}`;
+      const answer = await review.getAnswer(question);
+      const answerText = answer?.getAnswerText();
+      parsedReview[questionText] = answerText;
+    }
+
+    const reviewEvaluation = await review.getReviewOfThisReview();
+    const reviewEvaluationQuestionnaire = await reviewEvaluation?.getQuestionnaire();
+    let reviewEvaluationQuestions: Question[] = [];
+    if (reviewEvaluationQuestionnaire) {
+      reviewEvaluationQuestions = _.sortBy(
+        reviewEvaluationQuestionnaire.questions,
+        (question) => {
+          return question.number;
+        }
+      );
+    }
+    const reviewEvaluationReviewer = reviewEvaluation?.reviewer;
+
+    // Evaluator netid
+    parsedReview["Evaluator netid"] = reviewEvaluationReviewer?.netid;
+    // Evaluator studentnumber
+    parsedReview["Evaluator studentnumber"] =
+      reviewEvaluationReviewer?.studentNumber;
+    // Review evaluation started_at
+    parsedReview["Review evaluation started_at"] = reviewEvaluation?.startedAt;
+    // Review evaluation downloaded_at
+    parsedReview["Review evaluation downloaded_at"] =
+      reviewEvaluation?.downloadedAt;
+    // Review evaluation saved_at
+    parsedReview["Review evaluation saved_at"] = reviewEvaluation?.savedAt;
+    // Review evaluation submitted_at
+    parsedReview["Review evaluation submitted_at"] =
+      reviewEvaluation?.submittedAt;
+    // Review evaluation done
+    parsedReview["Review evaluation done"] = reviewEvaluation?.submitted;
+
+    // iterate over all questions
+    for (const question of reviewEvaluationQuestions) {
+      const questionText = `E${question.number}. ${question.text}`;
+      const answer = await reviewEvaluation?.getAnswer(question);
+      const answerText = answer?.getAnswerText();
+      parsedReview[questionText] = answerText;
+    }
+    parsedReviews.push(parsedReview);
+  }
+  return parsedReviews;
+};
+
+export default parseSubmissionReviewsForExport;
