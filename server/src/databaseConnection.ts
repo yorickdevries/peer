ef1e048f6fc1e39a2ca91227aaa862b0b377a316
Yorick <yorickdevries@live.com>
2020-07-19 21:58:26 +0200

    fix linter

--- server/src/databaseConnection.ts
+++ server/src/databaseConnection.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata"; // needed for typeORM to work
 import { createConnection, Connection } from "typeorm";
-import * as ormconfig from "./ormconfig"
+import * as ormconfig from "./ormconfig";
 
 const createDatabaseConnection = async function (): Promise<Connection> {
   return createConnection(ormconfig);
