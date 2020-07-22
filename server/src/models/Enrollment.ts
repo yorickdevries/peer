96c67ace9f8dcf6bd4476a53d95a6c5e8e02fee1
Yorick <yorickdevries@live.com>
2020-07-22 15:52:49 +0200

    add is Enum validator

--- server/src/models/Enrollment.ts
+++ server/src/models/Enrollment.ts
@@ -1,5 +1,5 @@
 import { Entity, PrimaryColumn, Column, ManyToOne, RelationId } from "typeorm";
-import { IsDefined } from "class-validator";
+import { IsDefined, IsEnum } from "class-validator";
 import BaseModel from "./BaseModel";
 import User from "./User";
 import Course from "./Course";
@@ -23,6 +23,7 @@ export default class Enrollment extends BaseModel {
 
   @Column()
   @IsDefined()
+  @IsEnum(UserRole)
   role: UserRole;
 
   constructor(user: User, course: Course, role: UserRole) {
