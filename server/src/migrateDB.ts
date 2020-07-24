5ffea52d1d31bd9f440b7275356e4287b46e22b9
Yorick <yorickdevries@live.com>
2020-07-24 19:19:18 +0200

    make mirateDB skeleton code

---
+++ server/src/migrateDB.ts
@@ -0,0 +1,9 @@
+const migrateDB = async function (): Promise<void> {
+  console.log("Start migration");
+
+  console.log("Done migration");
+  return;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+migrateDB();
