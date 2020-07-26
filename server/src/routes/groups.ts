d5dadc69e1ec5d33f0640111377026eff022aa8f
Yorick <yorickdevries@live.com>
2020-07-26 02:03:22 +0200

    thorw error if teacher is trying to delete a user which made a submissio

--- server/src/routes/groups.ts
+++ server/src/routes/groups.ts
@@ -156,6 +156,17 @@ router.patch(
     // check whether the user already has a group for one of the assignments
     const groupAssignments = await group.getAssignments();
     for (const assignment of groupAssignments) {
+      const submissions = await assignment.getSubmissions();
+      if (
+        _.some(submissions, (submission) => {
+          return submission.userNetid === user.netid;
+        })
+      ) {
+        res
+          .status(HttpStatusCode.FORBIDDEN)
+          .send("User has already made a submission for the assignment");
+        return;
+      }
       if (!assignment.isAtOrBeforeState(AssignmentState.SUBMISSION)) {
         res
           .status(HttpStatusCode.BAD_REQUEST)
