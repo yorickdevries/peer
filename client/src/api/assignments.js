33311116e388526d5d31483db1a1e39d0116873a
Yorick <yorickdevries@live.com>
2020-08-12 14:36:57 +0200

    add post of assignment route

--- client/src/api/assignments.js
+++ client/src/api/assignments.js
@@ -4,5 +4,49 @@ export default {
     get(courseId) {
         const params = { courseId: courseId }
         return client.get("assignments/", { params: params })
+    },
+    post(
+        name,
+        courseId,
+        reviewsPerUser,
+        enrollable,
+        reviewEvaluation,
+        publishDate,
+        dueDate,
+        reviewPublishDate,
+        reviewDueDate,
+        reviewEvaluationDueDate,
+        description,
+        externalLink,
+        file
+    ) {
+        // Create formData and append data
+        const formData = new FormData()
+        formData.append("name", name)
+        formData.append("courseId", courseId)
+        formData.append("reviewsPerUser", reviewsPerUser)
+        formData.append("enrollable", enrollable)
+        formData.append("reviewEvaluation", reviewEvaluation)
+        formData.append("publishDate", publishDate.toISOString())
+        formData.append("dueDate", dueDate.toISOString())
+        formData.append("reviewPublishDate", reviewPublishDate.toISOString())
+        formData.append("reviewDueDate", reviewDueDate.toISOString())
+        if (reviewEvaluationDueDate) {
+            formData.append("reviewEvaluationDueDate", reviewEvaluationDueDate.toISOString())
+        } else {
+            formData.append("reviewEvaluationDueDate", null)
+        }
+        if (!description) {
+            description = null
+        }
+        formData.append("description", description)
+        if (!externalLink) {
+            externalLink = null
+        }
+        formData.append("externalLink", externalLink)
+        if (file) {
+            formData.append("file", file)
+        }
+        return client.post("assignments/", formData)
     }
 }
