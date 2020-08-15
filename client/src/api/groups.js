df31b629653dad7b33f1933c7ac0dc441843decf
Yorick <yorickdevries@live.com>
2020-08-15 17:17:52 +0200

    Revise the group manager

--- client/src/api/groups.js
+++ client/src/api/groups.js
@@ -1,6 +1,28 @@
 import client from "./axiosClient"
 
 export default {
+    getAllForAssignment(assignmentId) {
+        const params = { assignmentId }
+        return client.get("groups", { params: params })
+    },
+    get(id) {
+        return client.get(`groups/${id}`)
+    },
+    post(assignmentId, name) {
+        const body = { assignmentId, name }
+        return client.post("groups", body)
+    },
+    delete(id) {
+        return client.delete(`groups/${id}`)
+    },
+    addUser(groupId, userNetid) {
+        const body = { userNetid }
+        return client.patch(`groups/${groupId}/adduser`, body)
+    },
+    removeUser(groupId, userNetid) {
+        const body = { userNetid }
+        return client.patch(`groups/${groupId}/removeuser`, body)
+    },
     import(assignmentId, file) {
         // Create formData and append data
         const formData = new FormData()
