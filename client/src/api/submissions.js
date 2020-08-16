cad95ac7aef47bc5d8f228a189b89d3a61741219
Yorick <yorickdevries@live.com>
2020-08-16 15:46:17 +0200

    Fix teacher overview of submissions

--- client/src/api/submissions.js
+++ client/src/api/submissions.js
@@ -1,6 +1,14 @@
 import client from "./axiosClient"
 
 export default {
+    get(assignmentId) {
+        const params = { assignmentId }
+        return client.get("submissions", { params: params })
+    },
+    getLatest(assignmentId) {
+        const params = { assignmentId }
+        return client.get("submissions/latest", { params: params })
+    },
     post(groupId, assignmentId, file, config) {
         // Create formData and append data
         const formData = new FormData()
