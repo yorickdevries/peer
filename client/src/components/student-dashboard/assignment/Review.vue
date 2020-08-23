840d18179b5b92be08fd7d224807615970c4b062
Yorick <yorickdevries@live.com>
2020-08-24 00:42:02 +0200

    only show pdf annotator in case the submission is a pdf

--- client/src/components/student-dashboard/assignment/Review.vue
+++ client/src/components/student-dashboard/assignment/Review.vue
@@ -46,7 +46,7 @@
                 </b-modal>
             </b-col>
         </b-row>
-        <PDFAnnotator :reviewId="reviewId"></PDFAnnotator>
+        <PDFAnnotator v-if="fileMetadata.extension === '.pdf'" :reviewId="reviewId"></PDFAnnotator>
 
         <!--Form, load only when answers are available-->
         <b-card v-if="answers" no-body class="mt-3">
