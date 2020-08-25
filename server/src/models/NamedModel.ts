10c73aa71d5bd7bebbbfb179fd28c85c6bab3c73
Yorick <yorickdevries@live.com>
2020-08-25 16:10:47 +0200

    change size of string primary keys

--- server/src/models/NamedModel.ts
+++ server/src/models/NamedModel.ts
@@ -5,7 +5,9 @@ import BaseModel from "./BaseModel";
 // This class is extended by other named models like Affiliation, Study and OrganisationUnit
 // as they are all classes with just a name as primary key
 export default abstract class NamedModel extends BaseModel {
-  @PrimaryColumn()
+  // length of 191 due to UTF-8MB4 encoding of strings
+  // see also: https://github.com/gogs/gogs/issues/4894#issuecomment-348861978
+  @PrimaryColumn({ length: 191 })
   @IsDefined()
   @IsString()
   @IsNotEmpty()
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -18,7 +18,9 @@ import parseNetID from "../util/parseNetID";
 
 @Entity()
 export default class User extends BaseModel {
-  @PrimaryColumn()
+  // length of 191 due to UTF-8MB4 encoding of strings
+  // see also: https://github.com/gogs/gogs/issues/4894#issuecomment-348861978
+  @PrimaryColumn({ length: 191 })
   @IsDefined()
   @IsString()
   @IsNotEmpty()
