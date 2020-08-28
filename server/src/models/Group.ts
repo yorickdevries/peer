3b48b4c2da8c6afffc5ca115f547538ab611d092
Yorick <yorickdevries@live.com>
2020-08-28 21:08:06 +0200

    fix group

--- server/src/models/Group.ts
+++ server/src/models/Group.ts
@@ -81,12 +81,7 @@ export default class Group extends BaseModel {
   }
 
   async getCourse(): Promise<Course> {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return (
-      await Group.findOneOrFail(this.id, {
-        relations: ["course"],
-      })
-    ).course!;
+    return Course.findOneOrFail(this.courseId);
   }
 
   async getUsers(): Promise<User[]> {
