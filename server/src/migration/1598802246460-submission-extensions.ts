d1130a474ee0a1ee56ffca38dc58a46373298771
Yorick <yorickdevries@live.com>
2020-08-30 17:53:26 +0200

    update migration script

--- server/src/migration/1598802246460-submission-extensions.ts
+++ server/src/migration/1598802246460-submission-extensions.ts
@@ -5,17 +5,10 @@ export class submissionExtensions1598802246460 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("ALTER TABLE `assignment` ADD `submissionExtensions` varchar(255) NOT NULL");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `publishDate` `publishDate` timestamp NOT NULL");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `dueDate` `dueDate` timestamp NOT NULL");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `reviewPublishDate` `reviewPublishDate` timestamp NOT NULL");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `reviewDueDate` `reviewDueDate` timestamp NOT NULL");
+        await queryRunner.query("UPDATE `assignment` SET `submissionExtensions` = '.pdf,.zip,.doc,.docx'");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `reviewDueDate` `reviewDueDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `reviewPublishDate` `reviewPublishDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `dueDate` `dueDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `publishDate` `publishDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'");
         await queryRunner.query("ALTER TABLE `assignment` DROP COLUMN `submissionExtensions`");
     }
 
