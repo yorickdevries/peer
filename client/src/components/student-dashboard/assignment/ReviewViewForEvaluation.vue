a81b98576b364bc0883f5c697f787708b16275bf
Yorick <yorickdevries@live.com>
2020-08-31 22:39:11 +0200

    move pdfannotator to separate row

--- client/src/components/student-dashboard/assignment/ReviewViewForEvaluation.vue
+++ client/src/components/student-dashboard/assignment/ReviewViewForEvaluation.vue
@@ -35,7 +35,15 @@
                 </dl>
             </b-col>
         </b-row>
-        <PDFAnnotator v-if="fileMetadata.extension === '.pdf'" :reviewId="reviewId" :readOnly="false"></PDFAnnotator>
+        <b-row>
+            <b-col>
+                <PDFAnnotator
+                    v-if="fileMetadata.extension === '.pdf'"
+                    :reviewId="reviewId"
+                    :readOnly="false"
+                ></PDFAnnotator>
+            </b-col>
+        </b-row>
 
         <!--Form, load only when answers are available-->
         <b-card v-if="answers" no-body class="mt-3">
