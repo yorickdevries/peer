56f51b2ffd0a2f5486eac2e568b27014986a1a16
Yorick <yorickdevries@live.com>
2020-09-05 21:04:06 +0200

    update passport

--- server/src/@types/index.d.ts
+++ server/src/@types/index.d.ts
@@ -1,5 +1,2 @@
-// needs to be updated to latest packages after migration
 declare module "file-system-cache";
 declare module "forwarded-for";
-declare module "passport-saml";
-declare module "passport-saml-metadata";
