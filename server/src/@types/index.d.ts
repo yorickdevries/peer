2d97dd8d11b5e8d80c66ea9934a3ff116923f465
Yorick <yorickdevries@live.com>
2020-09-07 22:18:10 +0200

    change to mysql2 and add extra print statements

--- server/src/@types/index.d.ts
+++ server/src/@types/index.d.ts
@@ -1,2 +1,3 @@
 declare module "file-system-cache";
 declare module "forwarded-for";
+declare module "mysql2";
--- server/src/middleware/authentication/checkAuthentication.ts
+++ server/src/middleware/authentication/checkAuthentication.ts
@@ -2,6 +2,26 @@ import { Request, Response, NextFunction } from "express";
 import HttpStatusCode from "../../enum/HttpStatusCode";
 import User from "../../models/User";
 
+import mysql from "mysql2";
+import config from "config";
+
+const databaseConfig: {
+  type: string;
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+  connectionUrl?: string;
+} = config.get("database");
+
+const connection = mysql.createConnection({
+  host: databaseConfig.host,
+  user: databaseConfig.username,
+  password: databaseConfig.password,
+  database: databaseConfig.database,
+});
+
 const checkAndSetAuthentication = async function (
   req: Request,
   res: Response,
@@ -10,7 +30,29 @@ const checkAndSetAuthentication = async function (
   if (req.isAuthenticated() && req.user?.netid) {
     // set req.user to the User object
     const netid = req.user.netid;
-    const user = await User.findOneOrFail(netid);
+
+    console.log("start0", req.originalUrl, netid, new Date());
+    connection.query(
+      `SELECT * FROM \`user\` WHERE \`netid\` = '${netid}'`,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+      (error: any, results: any, _fields: any) => {
+        if (error) {
+          console.log("error0", req.originalUrl, netid, new Date(), error);
+        } else {
+          console.log(results);
+          console.log("end0", req.originalUrl, netid, new Date());
+        }
+      }
+    );
+
+    let user;
+    console.log("start1", req.originalUrl, netid, new Date());
+    try {
+      user = await User.findOneOrFail(netid);
+    } catch (error) {
+      console.log("error1", req.originalUrl, netid, new Date(), error);
+    }
+    console.log("end1", req.originalUrl, netid, new Date());
     req.user = user;
     next();
   } else {
