b19be8f041a3486d149d0fe37fc92598d5b2efc7
Yorick <yorickdevries@live.com>
2020-09-09 01:06:37 +0200

    fix peer alive script

--- peer_alive.sh
+++ peer_alive.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 DATE=$(date +"%Y%m%d%H%M%S")
-curl --max-time 5 https://peer.tudelft.nl/api/faculties
+RESPONSECODE=$(curl --head --max-time 10 -s -o /dev/null -w "%{http_code}" https://peer.tudelft.nl/api/faculties)
+curl --max-time 10 https://peer.tudelft.nl/api/faculties
 
-if [ $? -eq 0 ]
+if [[ $? -eq 0 && $RESPONSECODE -eq 200 ]]
 then
-    echo "${DATE} alive" >> /data_nfs/peer/peer_alive.log
+    echo "${DATE} alive $? $RESPONSECODE" >> /data_nfs/peer/peer_alive.log
 else
-    echo "${DATE} dead" >> /data_nfs/peer/peer_alive.log
-    service peer restart
+    echo "${DATE} dead $? $RESPONSECODE" >> /data_nfs/peer/peer_alive.log
+    /usr/sbin/service peer restart
 fi
