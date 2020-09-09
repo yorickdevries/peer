d19e610474f0c9a732653ba566e126f4ff8f11a1
Yorick <yorickdevries@live.com>
2020-09-10 00:10:05 +0200

    enable debounce for the filters

--- client/src/components/teacher-dashboard/Groups.vue
+++ client/src/components/teacher-dashboard/Groups.vue
@@ -65,7 +65,7 @@
             <b-col cols="6" class="mb-3">
                 <b-form-group horizontal label="Filter" class="mb-0 mr-4">
                     <b-input-group>
-                        <b-form-input v-model="filter" placeholder="Type to search" />
+                        <b-form-input v-model="filter" debounce="1000" placeholder="Type to search" />
                         <b-input-group-append>
                             <b-btn :disabled="!filter" @click="filter = ''">Clear</b-btn>
                         </b-input-group-append>
