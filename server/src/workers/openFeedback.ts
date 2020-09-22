8d091769a6de6865826f1ff7b8e275721e3a2f2b
Yorick <yorickdevries@live.com>
2020-09-22 17:23:33 +0200

    count reviews while opening feedback

--- server/src/workers/openFeedback.ts
+++ server/src/workers/openFeedback.ts
@@ -15,18 +15,23 @@ const openFeedbackForAssignment = async function (
   }
   const submitted = false;
   const unsubmittedReviews = await questionnaire.getReviews(submitted);
+
+  //counters
+  let submittedCounter = 0;
+  let unsubmittedCounter = 0;
   // note: for every review a transaction is started to check review validity
   for (const review of unsubmittedReviews) {
     // try to submit every review
     try {
       await submitReview(review);
+      submittedCounter++;
     } catch (error) {
-      console.log(error);
+      unsubmittedCounter++;
     }
   }
   assignment.state = AssignmentState.FEEDBACK;
   await assignment.save();
-  return `Opened feedback for assignment ${assignment.id}`;
+  return `Opened feedback for assignment ${assignment.id}, ${submittedCounter} extra reviews are submitted, ${unsubmittedCounter} reviews are still unsubmitted`;
 };
 
 export default openFeedbackForAssignment;
