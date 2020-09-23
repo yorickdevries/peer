9759d995dd59b45e48f148e353e1fd15a63b6a64
Yorick <yorickdevries@live.com>
2020-09-23 16:28:27 +0200

    add logger on connections

--- server/src/middleware/authentication/checkAuthentication.ts
+++ server/src/middleware/authentication/checkAuthentication.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { getConnection } from "typeorm";
 import HttpStatusCode from "../../enum/HttpStatusCode";
 import User from "../../models/User";
 
@@ -8,6 +9,17 @@ const checkAndSetAuthentication = async function (
   next: NextFunction
 ): Promise<void> {
   if (req.isAuthenticated() && req.user?.netid) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const connection = getConnection().driver as any;
+    const pool = connection.pool;
+    const connectionLimit = pool.config.connectionLimit;
+    const allConnectionsLength = pool._allConnections.length;
+    const freeConnectionsLength = pool._freeConnections.length;
+    const queueConnectionsLength = pool._connectionQueue.length;
+    // debug info:
+    console.log(
+      `Connection info: Limit: ${connectionLimit}, All: ${allConnectionsLength}, Free: ${freeConnectionsLength} Queue: ${queueConnectionsLength}`
+    );
     // set req.user to the User object
     const netid = req.user.netid;
     const user = await User.findOneOrFail(netid);
