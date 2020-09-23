428c77d295b1ee6eccb494c047be9fbafdc2ed3a
Yorick <yorickdevries@live.com>
2020-09-23 16:34:33 +0200

    lock connection log in test

--- server/src/middleware/authentication/checkAuthentication.ts
+++ server/src/middleware/authentication/checkAuthentication.ts
@@ -9,17 +9,19 @@ const checkAndSetAuthentication = async function (
   next: NextFunction
 ): Promise<void> {
   if (req.isAuthenticated() && req.user?.netid) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const connection = getConnection().driver as any;
-    const pool = connection.pool;
-    const connectionLimit = pool.config.connectionLimit;
-    const allConnectionsLength = pool._allConnections.length;
-    const freeConnectionsLength = pool._freeConnections.length;
-    const queueConnectionsLength = pool._connectionQueue.length;
-    // debug info:
-    console.log(
-      `Connection info: Limit: ${connectionLimit}, All: ${allConnectionsLength}, Free: ${freeConnectionsLength} Queue: ${queueConnectionsLength}`
-    );
+    if (process.env.NODE_ENV !== "test") {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const connection = getConnection().driver as any;
+      const pool = connection.pool;
+      const connectionLimit = pool.config.connectionLimit;
+      const allConnectionsLength = pool._allConnections.length;
+      const freeConnectionsLength = pool._freeConnections.length;
+      const queueConnectionsLength = pool._connectionQueue.length;
+      // debug info:
+      console.log(
+        `Connection info: Limit: ${connectionLimit}, All: ${allConnectionsLength}, Free: ${freeConnectionsLength} Queue: ${queueConnectionsLength}`
+      );
+    }
     // set req.user to the User object
     const netid = req.user.netid;
     const user = await User.findOneOrFail(netid);
