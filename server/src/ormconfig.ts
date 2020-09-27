b1f9a54b8aa4b5e145ac892b65dd96d4365742b7
Yorick <yorickdevries@live.com>
2020-09-27 15:36:46 +0200

    set connectionlimit to 50 in production

--- server/src/ormconfig.ts
+++ server/src/ormconfig.ts
@@ -56,6 +56,8 @@ switch (databaseConfig.type) {
         username: databaseConfig.username,
         password: databaseConfig.password,
         database: databaseConfig.database,
+        // set connectionlimit to 1 in development to detetc deadlocks early
+        extra: { connectionLimit: process.env.NODE_ENV === "production" ? 50 : 1 },
       };
     }
     connectionConfig = { ...baseConfig, ...mariadbConfig };
