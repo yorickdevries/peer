37b3ac8d897413c92e7d58e187c157e8a6ca9ee4
Yorick <yorickdevries@live.com>
2020-10-03 15:36:59 +0200

    remove debug code

--- server/src/middleware/authentication/checkAuthentication.ts
+++ server/src/middleware/authentication/checkAuthentication.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from "express";
-import { getConnection } from "typeorm";
 import HttpStatusCode from "../../enum/HttpStatusCode";
 import User from "../../models/User";
 
@@ -9,19 +8,6 @@ const checkAndSetAuthentication = async function (
   next: NextFunction
 ): Promise<void> {
   if (req.isAuthenticated() && req.user?.netid) {
-    if (process.env.NODE_ENV !== "test") {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const connection = getConnection().driver as any;
-      const pool = connection.pool;
-      const connectionLimit = pool.config.connectionLimit;
-      const allConnectionsLength = pool._allConnections.length;
-      const freeConnectionsLength = pool._freeConnections.length;
-      const queueConnectionsLength = pool._connectionQueue.length;
-      // debug info:
-      console.log(
-        `Connection info: Limit: ${connectionLimit}, All: ${allConnectionsLength}, Free: ${freeConnectionsLength} Queue: ${queueConnectionsLength}`
-      );
-    }
     // set req.user to the User object
     const netid = req.user.netid;
     const user = await User.findOneOrFail(netid);
