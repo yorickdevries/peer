4d1b6cdea03b1da453bb5663251e7b11a14d320a
Yorick <yorickdevries@live.com>
2020-10-20 15:23:56 +0200

    remove

--- server/src/util/removePDFMetadata.ts
+++ server/src/util/removePDFMetadata.ts
@@ -4,14 +4,7 @@ import exiftoolBin from "dist-exiftool";
 const removePDFMetadata = async function (filePath: string): Promise<void> {
   const ep = new exiftool.ExiftoolProcess(exiftoolBin);
   await ep.open();
-  await ep
-    .writeMetadata(filePath, { all: "" }, ["overwrite_original"])
-    .then((data: unknown) => {
-      console.log("log: ", data);
-    })
-    .catch((data: unknown) => {
-      console.log("error: ", data);
-    });
+  await ep.writeMetadata(filePath, { all: "" }, ["overwrite_original"]);
   await ep.close();
 };
 
