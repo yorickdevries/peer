bbbabce6f1c7067fc9a58c44224fc770375bb887
Yorick <yorickdevries@live.com>
2020-10-20 14:18:06 +0200

    log info on file metadata

--- server/src/util/removePDFMetadata.ts
+++ server/src/util/removePDFMetadata.ts
@@ -1,11 +1,16 @@
 import exiftool from "node-exiftool";
 import exiftoolBin from "dist-exiftool";
+import fs from "fs"; //Load the filesystem module
 
 const removePDFMetadata = async function (filePath: string): Promise<void> {
+  const statsBefore = JSON.stringify(fs.statSync(filePath));
   const ep = new exiftool.ExiftoolProcess(exiftoolBin);
   await ep.open();
   await ep.writeMetadata(filePath, { all: "" }, ["overwrite_original"]);
   await ep.close();
+  const statsAfter = JSON.stringify(fs.statSync(filePath));
+  // log stats for debugging
+  console.log(`Before:\n${statsBefore}\nAfter\n${statsAfter}`);
 };
 
 export default removePDFMetadata;
