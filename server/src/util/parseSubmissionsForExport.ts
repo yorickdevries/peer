479d00ec1acc452de4d771561afe45a8a2e230ca
Yorick <yorickdevries@live.com>
2020-10-20 20:11:13 +0200

    extend export with more info

--- server/src/util/parseSubmissionsForExport.ts
+++ server/src/util/parseSubmissionsForExport.ts
@@ -8,10 +8,34 @@ const parseSubmissionsForExport = async function (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const parsedSubmissions: any[] = [];
   for (const submission of submissions) {
+    const submitter = await submission.getUser();
+    const submitterGroup = await submission.getGroup();
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const parsedSubmission: any = {};
-    // id
+    // id!: number;
     parsedSubmission["id"] = submission.id;
+    // Submitter netid
+    parsedSubmission["Submitter netid"] = submitter.netid;
+    // Submitter studentnumber
+    parsedSubmission["Submitter studentnumber"] = submitter.studentNumber;
+    // Submitter group id
+    parsedSubmission["Submitter group id"] = submitterGroup.id;
+    // Submitter group name
+    parsedSubmission["Submitter group name"] = submitterGroup.name;
+    // file
+    parsedSubmission["file"] = submission.file.name + submission.file.extension;
+    // final
+    parsedSubmission["final"] = submission.final;
+    // approvalByTA
+    parsedSubmission["approval by TA"] = submission.approvalByTA;
+    // approvingTA
+    parsedSubmission["TA netid"] = submission.approvingTA?.netid;
+    // created At
+    parsedSubmission["created at"] = submission.createdAt;
+    // updated At
+    parsedSubmission["updated at"] = submission.updatedAt;
+
     parsedSubmissions.push(parsedSubmission);
   }
   return parsedSubmissions;
