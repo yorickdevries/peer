172eee55d39a2cc0107317f004a4692a898a6952
Yorick <yorickdevries@live.com>
2020-11-05 19:51:25 +0100

    refine mailer method

--- server/src/util/mailer.ts
+++ server/src/util/mailer.ts
@@ -6,16 +6,23 @@ const transporter = nodemailer.createTransport({
   port: 25,
 });
 
-const sendStartupMail = async function (text: string): Promise<void> {
+const sendMailToAdmin = async function (
+  subject: string,
+  text: string
+): Promise<void> {
+  const message = {
+    from: "noreply@peer.tudelft.nl",
+    to: "y.c.devries-1@student.tudelft.nl",
+    subject: "[Peer] " + subject,
+    text: text,
+  };
   if (process.env.NODE_ENV === "production" && !isCI) {
-    const message = {
-      from: "noreply@peer.tudelft.nl",
-      to: "y.c.devries-1@student.tudelft.nl",
-      subject: "Startup mail from Peer",
-      text: text,
-    };
     await transporter.sendMail(message);
+  } else {
+    console.log(
+      `Mail that will be sent in production: ${JSON.stringify(message)}`
+    );
   }
 };
 
-export { sendStartupMail };
+export { sendMailToAdmin };
