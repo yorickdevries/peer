0ad2f69787a36b9a916103df41122aac60df87b6
Yorick <yorickdevries@live.com>
2020-11-05 22:16:19 +0100

    add close submission to separate method

--- server/src/assignmentProgression/closeSubmission.ts
+++ server/src/assignmentProgression/closeSubmission.ts
@@ -1,8 +1,22 @@
 import Assignment from "../models/Assignment";
+import { AssignmentState } from "../enum/AssignmentState";
 import { sendMailToAdmin } from "../util/mailer";
 
 const closeSubmissionHelper = async function (assignment: Assignment) {
-  return `TODO ${assignment}`;
+  if (!assignment.isAtState(AssignmentState.SUBMISSION)) {
+    throw new Error("The assignment is not in submission state");
+  }
+  for (const assignmentVersion of assignment.versions) {
+    const submissions = await assignmentVersion.getFinalSubmissionsOfEachGroup();
+    if (submissions.length === 0) {
+      throw new Error(
+        "There are no submissions for one of the assignment versions"
+      );
+    }
+  }
+  assignment.state = AssignmentState.WAITING_FOR_REVIEW;
+  await assignment.save();
+  return `Closed submissions for assignment: ${assignment.name} ID: ${assignment.id}`;
 };
 
 const closeSubmission = async function (
--- server/src/routes/assignments.ts
+++ server/src/routes/assignments.ts
@@ -22,6 +22,7 @@ import { AssignmentState } from "../enum/AssignmentState";
 import Extensions from "../enum/Extensions";
 import Submission from "../models/Submission";
 import publishAssignment from "../assignmentProgression/publishAssignment";
+import closeSubmission from "../assignmentProgression/closeSubmission";
 
 const router = express.Router();
 
@@ -553,24 +554,14 @@ router.patch(
         .send("User is not a teacher of the course");
       return;
     }
-    if (!assignment.isAtState(AssignmentState.SUBMISSION)) {
-      res
-        .status(HttpStatusCode.FORBIDDEN)
-        .send("The assignment is not in submission state");
+    try {
+      const result = await closeSubmission(assignment);
+      res.send(result);
+      return;
+    } catch (error) {
+      res.status(HttpStatusCode.FORBIDDEN).send(error);
       return;
     }
-    for (const assignmentVersion of assignment.versions) {
-      const submissions = await assignmentVersion.getFinalSubmissionsOfEachGroup();
-      if (submissions.length === 0) {
-        res
-          .status(HttpStatusCode.FORBIDDEN)
-          .send("There are no submissions for one of the assignment versions");
-        return;
-      }
-    }
-    assignment.state = AssignmentState.WAITING_FOR_REVIEW;
-    await assignment.save();
-    res.send(assignment);
   }
 );
 
