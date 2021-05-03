4684e041ed753e7d29e74a798fad0eacdc9bf5f3
Vincent <v.groen@student.tudelft.nl>
2021-05-03 17:21:14 +0200

    Add code view component
    This commit adds a basic code view component. This component has two
    props, one for the string of code and one to specify the language or
    file extension.

---
+++ client/src/components/general/CodeViewer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <pre><code v-html="content"></code></pre>
+    </div>
+</template>
+
+<script>
+import hljs from "highlight.js"
+import "highlight.js/styles/default.css"
+export default {
+    props: ["code", "ext"],
+    data() {
+        return {
+            content: null
+        }
+    },
+    created() {
+        // hljs.highlightAuto expects a string (code) and optionally an array
+        // of language names / aliases
+        const highlighted = hljs.highlightAuto(this.code, [this.ext])
+        this.content = highlighted.value
+    }
+}
+</script>
