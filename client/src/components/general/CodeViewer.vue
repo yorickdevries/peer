76f68bae0c803f9c39885b5390b88896537cca97
Vincent <v.groen@student.tudelft.nl>
2021-05-03 21:54:37 +0200

    Change CodeViewer props
    This commit changes the props that the CodeViewer component takes in
    order to be more similar to the PDFViewer. Instead of taking text and an
    extension, it now takes a url. It retrieves the text and tries to
    recognize the language by itself.

--- client/src/components/general/CodeViewer.vue
+++ client/src/components/general/CodeViewer.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <pre><code v-html="content"></code></pre>
+        <pre><code style="display: block" v-for="(line, index) in content" :key="index" v-html="line"></code></pre>
     </div>
 </template>
 
@@ -8,17 +8,22 @@
 import hljs from "highlight.js"
 import "highlight.js/styles/default.css"
 export default {
-    props: ["code", "ext"],
+    props: ["fileUrl"],
     data() {
         return {
             content: null
         }
     },
     created() {
-        // hljs.highlightAuto expects a string (code) and optionally an array
-        // of language names / aliases
-        const highlighted = hljs.highlightAuto(this.code, [this.ext])
-        this.content = highlighted.value
+        fetch(this.fileUrl)
+            .then(response => response.text())
+            .then(text => {
+                // hljs.highlightAuto expects a string (code) and optionally an array
+                // of language names / aliases
+                const highlighted = hljs.highlightAuto(text)
+                this.content = highlighted.value.split(/\r?\n/g)
+            })
+            .catch(console.warn)
     }
 }
 </script>
