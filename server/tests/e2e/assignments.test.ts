aa06a0beb80fb66a7fa2e41f4ff835105e9c8c8c
Max Rensen <m.l.rensen@student.tudelft.nl>
2021-05-07 16:16:46 +0200

    Add more tests
    This adds some more tests after recommendations from the MR

--- server/tests/e2e/assignments.test.ts
+++ server/tests/e2e/assignments.test.ts
@@ -86,7 +86,34 @@ describe("Assignments", () => {
     });
   });
 
-  test("patch assignment with changed type", async () => {
+  test("make an assignment with invalid assignment type", async () => {
+    const res2 = await request(server)
+      .post("/api/assignments")
+      .set("cookie", sessionCookie)
+      .attach("file", fs.readFileSync(exampleAssignmentFile), "assignment1.pdf")
+      .field("name", "Example title")
+      .field("courseId", course.id)
+      .field("enrollable", false)
+      .field("reviewEvaluation", false)
+      .field("publishDate", new Date("2020-06-23T10:00Z").toISOString())
+      .field("dueDate", new Date("2020-06-24T10:00Z").toISOString())
+      .field("reviewPublishDate", new Date("2020-06-25T10:00Z").toISOString())
+      .field("reviewDueDate", new Date("2020-06-26T10:00Z").toISOString())
+      .field("reviewEvaluationDueDate", "null")
+      .field("description", "Example description")
+      .field("externalLink", "null")
+      .field("submissionExtensions", ".pdf")
+      .field("blockFeedback", true)
+      .field("lateSubmissions", true)
+      .field("lateSubmissionReviews", true)
+      .field("lateReviewEvaluations", "null")
+      .field("automaticStateProgression", false)
+      .field("assignmentType", "invalid");
+
+    expect(res2.status).toBe(HttpStatusCode.BAD_REQUEST);
+  });
+
+  test("invalid patch assignment with changed type", async () => {
     const res2 = await request(server)
       .post("/api/assignments")
       .set("cookie", sessionCookie)
@@ -146,5 +173,66 @@ describe("Assignments", () => {
       .field("assignmentType", AssignmentType.CODE);
 
     expect(res4.status).toBe(HttpStatusCode.FORBIDDEN);
+  });
+
+  test("valid patch assignment with changed type", async () => {
+    const res2 = await request(server)
+      .post("/api/assignments")
+      .set("cookie", sessionCookie)
+      .attach("file", fs.readFileSync(exampleAssignmentFile), "assignment1.pdf")
+      .field("name", "Example title")
+      .field("courseId", course.id)
+      .field("enrollable", false)
+      .field("reviewEvaluation", false)
+      .field("publishDate", new Date("2020-06-23T10:00Z").toISOString())
+      .field("dueDate", new Date("2020-06-24T10:00Z").toISOString())
+      .field("reviewPublishDate", new Date("2020-06-25T10:00Z").toISOString())
+      .field("reviewDueDate", new Date("2020-06-26T10:00Z").toISOString())
+      .field("reviewEvaluationDueDate", "null")
+      .field("description", "Example description")
+      .field("externalLink", "null")
+      .field("submissionExtensions", ".pdf")
+      .field("blockFeedback", true)
+      .field("lateSubmissions", true)
+      .field("lateSubmissionReviews", true)
+      .field("lateReviewEvaluations", "null")
+      .field("automaticStateProgression", false)
+      .field("assignmentType", AssignmentType.DOCUMENT);
+
+    const assignment = JSON.parse(res2.text);
+    const res3 = await request(server)
+      .post("/api/assignmentversions")
+      .send({
+        name: "Example version",
+        assignmentId: assignment.id,
+        reviewsPerUserPerAssignmentVersionToReview: 1,
+      })
+      .set("cookie", sessionCookie);
+
+    expect(res3.status).toBe(HttpStatusCode.OK);
+
+    const res4 = await request(server)
+      .patch(`/api/assignments/${assignment.id}`)
+      .set("cookie", sessionCookie)
+      .attach("file", fs.readFileSync(exampleAssignmentFile), "assignment1.pdf")
+      .field("name", "Example title")
+      .field("enrollable", false)
+      .field("reviewEvaluation", false)
+      .field("publishDate", new Date("2020-06-23T10:00Z").toISOString())
+      .field("dueDate", new Date("2020-06-24T10:00Z").toISOString())
+      .field("reviewPublishDate", new Date("2020-06-25T10:00Z").toISOString())
+      .field("reviewDueDate", new Date("2020-06-26T10:00Z").toISOString())
+      .field("reviewEvaluationDueDate", "null")
+      .field("description", "Example description")
+      .field("externalLink", "null")
+      .field("submissionExtensions", ".pdf")
+      .field("blockFeedback", true)
+      .field("lateSubmissions", true)
+      .field("lateSubmissionReviews", true)
+      .field("lateReviewEvaluations", "null")
+      .field("automaticStateProgression", false)
+      .field("assignmentType", AssignmentType.CODE);
+
+    expect(res4.status).toBe(HttpStatusCode.OK);
   });
 });
