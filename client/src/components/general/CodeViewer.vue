b2a9421b708b8ab6c484c2f5023507a26f0543c5
Vincent <v.groen@student.tudelft.nl>
2021-05-14 10:51:30 +0200

    Add line numbers to the CodeViewer
    This commit adds line numbers to the CodeViewer component. It also
    resolves #39, because otherwise the line numbers would not line up with
    the lines themselves.

--- client/src/components/general/CodeViewer.vue
+++ client/src/components/general/CodeViewer.vue
@@ -1,11 +1,53 @@
 <template>
-    <pre>
-        <code style="display: block" v-for="(line, index) in content" :key="index" v-html="line"></code>
-    </pre>
+    <div>
+        <pre>
+            <div v-for="(line, index) in content" :key="index + 'line'">
+                <code v-bind:style="{ width: `${maxLineNumberDigits}ch` }">{{ index + 1 }}</code>
+            </div>
+        </pre>
+        <pre>
+            <div v-for="(line, index) in content" :key="index + 'code'">
+                <code v-html="line.replace(/^$/, '<br />')"></code>
+            </div>
+        </pre>
+    </div>
 </template>
 
 <script>
 export default {
-    props: ["content"]
+    props: ["content"],
+    computed: {
+        maxLineNumberDigits() {
+            return Math.ceil(Math.log(this.content.length + 1) / Math.log(10))
+        }
+    }
 }
 </script>
+
+<style lang="scss" scoped>
+pre {
+    white-space: pre-line;
+    display: inline-block;
+
+    div {
+        white-space: initial;
+
+        code {
+            white-space: pre;
+        }
+    }
+
+    &:first-of-type {
+        div {
+            code {
+                display: inline-block;
+                text-align: right;
+                box-sizing: content-box;
+                padding-right: 1ch;
+                margin-right: 1ch;
+                border-right: 1px solid var(--gray);
+            }
+        }
+    }
+}
+</style>
