61413e4c309cbd6bed84413421bc685f824eb68d
Thomas Baars <T.C.Baars@student.tudelft.nl>
2021-05-14 17:16:58 +0200

    Update hotfix to allow initial submissions

--- client/src/components/student-dashboard/assignment/Submission.vue
+++ client/src/components/student-dashboard/assignment/Submission.vue
@@ -140,14 +140,13 @@
             <b-row>
                 <b-col>
                     <dt>You can view your final submission here:</dt>
-                    <div>
+                    <div v-if="finalSubmission">
                         <b-alert v-if="finalSubmission && finalSubmission.file.extension === '.pdf'" variant="secondary"
                             >In case the viewer shows any errors, your .pdf is malformed and no pdf annotations can be
                             made by your reviewers directly in the browser. Reviewers can always download the file
                             instead.</b-alert
                         >
                         <FileAnnotator
-                            :v-if="finalSubmission"
                             :submissionId="finalSubmission.id"
                             :readOnly="true"
                             :assignmentType="assignment.assignmentType"
