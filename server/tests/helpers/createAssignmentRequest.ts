b7054571788d6c5348db8fd1e88e27ffc7aaf24e
Max Rensen <m.l.rensen@student.tudelft.nl>
2021-05-17 23:16:15 +0200

    Add tests for submission extensions validation
    This adds tests for the newly added submission extensions validation.

---
+++ server/tests/helpers/createAssignmentRequest.ts
@@ -0,0 +1,36 @@
+import http from "http";
+import request from "supertest";
+import fs from "fs";
+import AssignmentType from "../../src/enum/AssignmentType";
+
+export default async function createAssignmentRequest(
+  server: http.Server,
+  courseId: number,
+  exampleAssignmentFile: string,
+  sessionCookie: string,
+  assignmentType?: string,
+  submissionExtensions?: string
+): Promise<request.Response> {
+  return await request(server)
+    .post("/api/assignments")
+    .set("cookie", sessionCookie)
+    .attach("file", fs.readFileSync(exampleAssignmentFile), "assignment1.pdf")
+    .field("name", "Example title")
+    .field("courseId", courseId)
+    .field("enrollable", false)
+    .field("reviewEvaluation", false)
+    .field("publishDate", new Date("2020-06-23T10:00Z").toISOString())
+    .field("dueDate", new Date("2020-06-24T10:00Z").toISOString())
+    .field("reviewPublishDate", new Date("2020-06-25T10:00Z").toISOString())
+    .field("reviewDueDate", new Date("2020-06-26T10:00Z").toISOString())
+    .field("reviewEvaluationDueDate", "null")
+    .field("description", "Example description")
+    .field("externalLink", "null")
+    .field("submissionExtensions", submissionExtensions ?? ".pdf")
+    .field("blockFeedback", true)
+    .field("lateSubmissions", true)
+    .field("lateSubmissionReviews", true)
+    .field("lateReviewEvaluations", "null")
+    .field("automaticStateProgression", false)
+    .field("assignmentType", assignmentType ?? AssignmentType.DOCUMENT);
+}
