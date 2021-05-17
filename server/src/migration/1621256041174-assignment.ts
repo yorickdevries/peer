785b44ae6bc99de72d62e499f9feb7e2f99a6267
Adriaan Brands <a.j.m.brands@student.tudelft.nl>
2021-05-17 16:42:08 +0200

    Removed unneccessary lines from assignment migration script

--- server/src/migration/1621256041174-assignment.ts
+++ server/src/migration/1621256041174-assignment.ts
@@ -4,20 +4,12 @@ export class assignment1621256041174 implements MigrationInterface {
     name = 'assignment1621256041174'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `publishDate` `publishDate` timestamp NOT NULL");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `dueDate` `dueDate` timestamp NOT NULL");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `reviewPublishDate` `reviewPublishDate` timestamp NOT NULL");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `reviewDueDate` `reviewDueDate` timestamp NOT NULL");
         // Notice how it says 'ADD' instead of 'CHANGE'. This makes sure the column assignmentType gets initialized with "document"
         await queryRunner.query("ALTER TABLE `assignment` ADD `assignmentType` varchar(255) NOT NULL DEFAULT 'document'");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("ALTER TABLE `assignment` CHANGE `assignmentType` `assignmentType` varchar(255) NOT NULL");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `reviewDueDate` `reviewDueDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `reviewPublishDate` `reviewPublishDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `dueDate` `dueDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'");
-        await queryRunner.query("ALTER TABLE `assignment` CHANGE `publishDate` `publishDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'");
     }
 
 }
