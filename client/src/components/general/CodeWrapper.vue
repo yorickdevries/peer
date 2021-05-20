b42cb11ee9c785db26f4cdd1cb61f95978c65265
Adriaan Brands <a.j.m.brands@student.tudelft.nl>
2021-05-20 17:36:35 +0200

    Start off FileTree component as collapsed with single file

--- client/src/components/general/CodeWrapper.vue
+++ client/src/components/general/CodeWrapper.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="files" class="d-flex">
-        <div v-if="!singleFile" style="flex-shrink: 0; max-width: 40%">
-            <FileTree :files="files" :selectedFile="selected" @selected="onSelect" />
+        <div style="flex-shrink: 0; max-width: 40%">
+            <FileTree :files="files" :selectedFile="selected" @selected="onSelect" :startCollapsed="singleFile" />
         </div>
         <div
             v-bind:class="{ 'ml-3': !singleFile }"
