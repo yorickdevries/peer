6c8395535b8490dae2d42b7223d6e775bccfc9f9
Thomas Baars <T.C.Baars@student.tudelft.nl>
2021-06-26 18:21:03 +0200

    Remove Close button and redundant text

--- client/src/components/teacher-dashboard/ImportSubmissionsWizard.vue
+++ client/src/components/teacher-dashboard/ImportSubmissionsWizard.vue
@@ -1,32 +1,27 @@
 <template>
     <b-card header="Import Submissions">
         <b-alert class="d-flex justify-content-between flex-wrap" show variant="primary">
+            <p>The importing of submissions can <b>only</b> be performed when:</p>
+            <ul>
+                <li>No groups or submissions already exist for the assignmnet.</li>
+                <li>
+                    The corresponding students must already exist in the Peer database, i.e. they must have logged into
+                    Peer at least once.
+                </li>
+                <li>The file size at most 50 MB.</li>
+            </ul>
             <p>To import submissions for an assignment from WebLab:</p>
-            <ol>
+            <ul>
                 <li>
                     Manually export submissions by downloading them via the <b>"Actions"</b> menu, located at the top
                     right-hand corner of the target assignment webpage on WebLab, where there is an option to
                     <b>"Download Submissions"</b>.
                 </li>
                 <li>
-                    Format the downloaded submissions into a single zip file, that has a folder called
-                    <b>"submissions"</b> containing all submissions. Where each submission should be in a separate
-                    subfolder using the following format: <b>"StudentNumber_FirstName_LastName"</b>.
+                    The downloaded submissions should be in a single zip file, that has a folder called
+                    <b>"submissions"</b> containing all submissions. Where each submission is in a separate subfolder
+                    using the following format: <b>"StudentNumber_FirstName_LastName"</b>.
                 </li>
-                <li>
-                    Use the file browser below to locate and upload the formatted zip file.
-                </li>
-            </ol>
-            <p>The importing of submissions can <b>only</b> be performed when:</p>
-            <ul>
-                <li>The assignment is not passed the Submission stage.</li>
-                <li>The assignment is not self enrollable.</li>
-                <li>No groups or submissions already exist for the assignmnet.</li>
-                <li>
-                    The corresponding students must already exist in the Peer database, i.e. they must have logged into
-                    Peer at least once.
-                </li>
-                <li>The file size of the formatted zip file is at most 50 MB.</li>
             </ul>
             <p>
                 Note: It may take time for the imported submissions to appear in the "Submissions" tab. If the import
@@ -43,12 +38,7 @@
                 :state="Boolean(file)"
             >
             </b-form-file>
-            <div class="modal-footer">
-                <b-button variant="primary" class="mt-2 mr-auto" @click="importSubmissions">
-                    Import submissions
-                </b-button>
-                <b-button variant="secondary" class="mt-2" @click="closeModal">Close</b-button>
-            </div>
+            <b-button variant="primary" class="mt-3" @click="importSubmissions">Import submissions</b-button>
         </b-form-group>
     </b-card>
 </template>
@@ -70,9 +60,7 @@ export default {
             this.disableSubmissionImportButton = true
             await api.submissions.import(this.assignmentVersionId, this.file)
             this.showSuccessMessage({ message: "Submissions are being imported" })
-            this.closeModal()
-        },
-        async closeModal() {
+            // Close modal after submitting
             this.$bvModal.hide(this.modalId)
         }
     }
