4fafd39a7f8b070d90d5fe542a881ef7517fecbb
Yorick <yorickdevries@live.com>
2021-07-12 15:18:51 +0200

    add script to create academic year

---
+++ server/src/scripts/createData.ts
@@ -0,0 +1,34 @@
+import AcademicYear from "../models/AcademicYear";
+import createDatabaseConnection from "../databaseConnection";
+
+// boolean which indicates whether the script can be run, can se set to True temporarily
+const runScript = false;
+
+const createData = async function (): Promise<void> {
+  // Academic Year
+  await new AcademicYear("2021/2022", true).save();
+  return;
+};
+
+// initialize data in case of running in development
+if (!runScript) {
+  throw new Error(`Not allowed to run script`);
+} else {
+  createDatabaseConnection()
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    .then((_connection) => {
+      createData()
+        .then(() => {
+          console.log("Initialized data");
+          process.exit(0);
+        })
+        .catch((error) => {
+          console.error(error);
+          process.exit(1);
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+}
