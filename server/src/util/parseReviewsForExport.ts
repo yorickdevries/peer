8ea09e9f1e591c191a74bcbec6f429f93be6827b
Lohithsai <lyadalachanchu>
2022-09-19 18:32:29 +0200

    Fix mocking issues

--- server/src/util/parseReviewsForExport.ts
+++ server/src/util/parseReviewsForExport.ts
@@ -14,11 +14,9 @@ const parseSubmissionReviewsForExport = async function (
   const questions = _.sortBy(submissionQuestionnaire.questions, (question) => {
     return question.number;
   });
-
   const reviews = (await submissionQuestionnaire.getReviews()) as ReviewOfSubmission[];
   const assignmentVersion = await submissionQuestionnaire.getAssignmentVersion();
   const assignment = await assignmentVersion.getAssignment();
-
   // the parsed review is too extensive to make an interface
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const parsedReviews: any[] = [];
--- server/tests/unit/gradesExport.test.ts
+++ server/tests/unit/gradesExport.test.ts
@@ -6,7 +6,7 @@ import ReviewOfSubmission from "../../src/models/ReviewOfSubmission";
 import Submission from "../../src/models/Submission";
 import SubmissionQuestionnaire from "../../src/models/SubmissionQuestionnaire";
 import User from "../../src/models/User";
-import { mock, when } from "ts-mockito";
+import { mock, when, instance } from "ts-mockito";
 import Assignment from "../../src/models/Assignment";
 import AssignmentType from "../../src/enum/AssignmentType";
 import PDFAnnotation from "../../src/models/PDFAnnotation";
@@ -14,80 +14,118 @@ import MultipleChoiceQuestionAnswer from "../../src/models/MultipleChoiceQuestio
 import MultipleChoiceQuestionOption from "../../src/models/MultipleChoiceQuestionOption";
 import ReviewOfReview from "../../src/models/ReviewOfReview";
 import parseSubmissionReviewsForExport from "../../src/util/parseReviewsForExport";
+import { Connection } from "typeorm";
+import createDatabaseConnection from "../../src/databaseConnection";
+import initializeData from "../../src/util/initializeData";
 
 describe("Single MCQ Question", () => {
+  // will be initialized and closed in beforeAll / afterAll
+  let connection: Connection;
+
+  beforeAll(async () => {
+    // For the in memory test database, the schema is automatically dropped upon connect
+    connection = await createDatabaseConnection();
+    await initializeData();
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
   const mockedQuestionnaire: SubmissionQuestionnaire = mock(
     SubmissionQuestionnaire
   );
+  const instanceOfQ = instance(mockedQuestionnaire)
   const m = new MultipleChoiceQuestion(
     "this is mcq",
     1,
     false,
     true,
-    mockedQuestionnaire
+    instanceOfQ
   );
   m.options = [
     new MultipleChoiceQuestionOption("correct", m, 1),
     new MultipleChoiceQuestionOption("wrong", m, 0),
   ];
-  mockedQuestionnaire.questions = [m];
+  instanceOfQ.questions = [m];
   const g1: Group = mock(Group);
-  g1.id = 1;
-  g1.name = "g1";
+  const instanceOfG1 = instance(g1)
+  instanceOfG1.id = 1;
+  instanceOfG1.name = "g1";
   //submitter
-  const u1: User = new User("u1");
-  u1.studentNumber = 1;
-  //reviewer
-  const u2: User = new User("u2");
-  u2.studentNumber = 2;
-  const s1: Submission = mock(Submission);
+  const instanceOfU1: User = new User("u1");
 
-  when(s1.getUser()).thenReturn(
-    new Promise<User>((resolve) => {
-      resolve(u1);
-    })
-  );
-  when(s1.getGroup()).thenReturn(
-    new Promise<Group>((resolve) => {
-      resolve(g1);
-    })
-  );
+  //instanceOfU1.studentNumber = 1;
+  //reviewer
+  const instanceOfU2: User = new User("u2");
+  //instanceOfU2.studentNumber = 2;
+  const s1: Submission = mock(Submission);
+  const instanceOfS1 = instance(s1)
+
 
   const r1: ReviewOfSubmission = mock(ReviewOfSubmission);
-  const ma = new MultipleChoiceQuestionAnswer(
-    m,
-    r1,
-    new MultipleChoiceQuestionOption("correct", m, 1)
-  );
+  const instanceOfR1 = instance(r1);
+
+  instanceOfR1.submission = instanceOfS1;
+  instanceOfR1.reviewer = instanceOfU2;
+  instanceOfR1.id = 1;
+  instanceOfR1.submitted = true;
+  instanceOfR1.flaggedByReviewer = false;
+  const ma = mock(MultipleChoiceQuestionAnswer);
+
+  const answerOption = mock(MultipleChoiceQuestionOption);
+  const instanceOfAnswerOption = instance(answerOption);
+  instanceOfAnswerOption.text = "correct";
+  instanceOfAnswerOption.question = m;
+  instanceOfAnswerOption.points = 1;
+
+  const instanceOfMa = instance(ma)
+  instanceOfMa.question = m;
+  instanceOfMa.review = instanceOfR1;
+  instanceOfMa.multipleChoiceAnswer = instanceOfAnswerOption;
+  // const ma = new MultipleChoiceQuestionAnswer(
+  //   m,
+  //   instanceOfR1,
+  //   new MultipleChoiceQuestionOption("correct", m, 1)
+  // );
   const a1: Assignment = mock(Assignment);
-  when(a1.getGroup(u2)).thenReturn(
-    new Promise<Group>((resolve) => {
-      resolve(g1);
-    })
-  );
+  const instanceOfA1: Assignment = instance(a1)
+
   //assignment is a pdf
-  a1.assignmentType = AssignmentType.DOCUMENT;
-  r1.submission = s1;
-  r1.reviewer = u2;
-  r1.id = 1;
-  r1.submitted = true;
-  r1.flaggedByReviewer = false;
+  instanceOfA1.assignmentType = AssignmentType.DOCUMENT;
+  
   const pdfA: PDFAnnotation = mock(PDFAnnotation);
-  when(r1.getPDFAnnotations()).thenReturn(
-    new Promise<PDFAnnotation[]>((resolve) => {
-      resolve([pdfA]);
-    })
-  );
+  
   const reviewSubmissionList: Promise<Review[]> = new Promise<Review[]>(
     (resolve) => {
-      const lst: ReviewOfSubmission[] = [r1];
+      const lst: ReviewOfSubmission[] = [instanceOfR1];
       resolve(lst);
     }
   );
   const aVersion = mock(AssignmentVersion);
+
+  when(s1.getUser()).thenReturn(
+    new Promise<User>((resolve) => {
+      resolve(instanceOfU1);
+    })
+  );
+  when(s1.getGroup()).thenReturn(
+    new Promise<Group>((resolve) => {
+      resolve(instanceOfG1);
+    })
+  );
+  when(a1.getGroup(instanceOfU2)).thenReturn(
+    new Promise<Group>((resolve) => {
+      resolve(instanceOfG1);
+    })
+  );
+  when(r1.getPDFAnnotations()).thenReturn(
+    new Promise<PDFAnnotation[]>((resolve) => {
+      resolve([instance(pdfA)]);
+    })
+  );
   when(r1.getAnswer(m)).thenReturn(
     new Promise<MultipleChoiceQuestionAnswer>((resolve) => {
-      resolve(ma);
+      resolve(instanceOfMa);
     })
   );
   when(r1.getReviewOfThisReview()).thenReturn(
@@ -97,14 +135,17 @@ describe("Single MCQ Question", () => {
   );
   when(aVersion.getAssignment()).thenReturn(
     new Promise<Assignment>((resolve) => {
-      resolve(a1);
+      resolve(instanceOfA1);
     })
   );
   when(mockedQuestionnaire.getReviews()).thenReturn(reviewSubmissionList);
   when(mockedQuestionnaire.getAssignmentVersion()).thenReturn(
     new Promise<AssignmentVersion>((resolve) => {
-      resolve(aVersion);
+      resolve(instance(aVersion));
     })
   );
-  expect(parseSubmissionReviewsForExport(mockedQuestionnaire)).toBe("normalnetid");
+
+
+
+  expect(parseSubmissionReviewsForExport(instanceOfQ)).toBe({});
 });
