cf9a03c5e7f60562afd606fb3dadb1e32727d928
Lohithsai <lyadalachanchu>
2022-09-22 20:26:25 +0200

    Make writing to zip file asynchronous

--- server/src/util/exportZip.ts
+++ server/src/util/exportZip.ts
@@ -22,7 +22,7 @@ const exportToZip = async function (
   }
 
   const content = await zip.generateAsync({ type: "nodebuffer" });
-  fs.writeFileSync(`${fileName}.zip`, content);
+  fs.writeFile(`${fileName}.zip`, content, () => {console.log(`Written ${fileName} to zip`)});
   const file = new File(
     `${fileName}`,
     ".zip",
