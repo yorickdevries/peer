41a8a7bc761ec8e6947ca3552f218c1392d5c789
Philip De Munck <P.L.DeMunck@student.tudelft.nl>
2022-11-12 18:08:33 +0100

    Change link, enable auto save

--- client/src/components/student-dashboard/assignment/PDFAnnotator.vue
+++ client/src/components/student-dashboard/assignment/PDFAnnotator.vue
@@ -95,7 +95,7 @@ export default {
             // Documentation: https://www.adobe.io/apis/documentcloud/dcsdk/docs.html
             // inject the PDF embed API script
             const script = document.createElement("script")
-            script.setAttribute("src", "https://documentcloud.adobe.com/view-sdk/main.js")
+            script.setAttribute("src", "https://documentservices.adobe.com/view-sdk/viewer.js")
             document.head.appendChild(script)
 
             // set vue model to constant
@@ -154,13 +154,39 @@ export default {
                     enableAnnotationAPIs: true,
                     includePDFAnnotations: true
                 }
+                const metaData = {
+                    fileName: vm.reviewFileName,
+                    id: String(vm.fileMetadata.id) // id needs to be a string
+                }
+
+                //Set auto save
+                const saveOptions = {
+                    autoSaveFrequency: 1,
+                    enableFocusPolling: true,
+                    showSaveButton: false
+                }
+
+                adobeDCView.registerCallback(
+                    // eslint-disable-next-line no-undef
+                    AdobeDC.View.Enum.CallbackType.SAVE_API,
+                    function() {
+                        return new Promise(resolve => {
+                            resolve({
+                                // eslint-disable-next-line no-undef
+                                code: AdobeDC.View.Enum.ApiResponseCode.SUCCESS,
+                                data: {
+                                    metaData
+                                }
+                            })
+                        })
+                    },
+                    saveOptions
+                )
+
                 const previewFilePromise = adobeDCView.previewFile(
                     {
                         content: { promise: filePromise },
-                        metaData: {
-                            fileName: vm.reviewFileName,
-                            id: String(vm.fileMetadata.id) // id needs to be a string
-                        }
+                        metaData
                     },
                     previewConfig
                 )
