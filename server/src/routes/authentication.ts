fbfe68fe2d8430c7ea2a57db9b47bf36a860ab17
Philip De Munck <P.L.DeMunck@student.tudelft.nl>
2022-11-15 15:40:53 +0100

    Add test bypass

--- server/src/routes/authentication.ts
+++ server/src/routes/authentication.ts
@@ -1,9 +1,12 @@
+import Joi from "@hapi/joi";
 import { Router } from "express";
 import config from "config";
+import mockPassportConfiguration from "../middleware/authentication/passportMock";
 import passport from "passport";
 import passportConfiguration from "../middleware/authentication/passportTUDelft";
 import path from "path";
 import session from "../middleware/authentication/session";
+import { validateBody } from "../middleware/validation";
 
 // Adds authentication routes
 const authenticationRoutes = function (router: Router): void {
@@ -14,44 +17,69 @@ const authenticationRoutes = function (router: Router): void {
   router.use(passport.session());
 
   // Depending of current mode, setup the login method
-  // Initialize TU Delft passport
-  passportConfiguration(passport);
+  if (process.env.NODE_ENV !== "test") {
+    // Initialize TU Delft passport
+    passportConfiguration(passport);
 
-  const url: string = config.get("url");
-  // Login route
-  router.get(
-    "/login",
-    passport.authenticate("saml", {
-      successRedirect: url,
-      failureRedirect: "/login",
-    })
-  );
+    const url: string = config.get("url");
+    // Login route
+    router.get(
+      "/login",
+      passport.authenticate("saml", {
+        successRedirect: url,
+        failureRedirect: "/login",
+      })
+    );
 
-  // Callback of the login route
-  router.post(
-    "/login/callback",
-    passport.authenticate("saml", {
-      failureRedirect: "/",
-      failureFlash: true,
-    }),
-    (_req, res) => {
+    // Callback of the login route
+    router.post(
+      "/login/callback",
+      passport.authenticate("saml", {
+        failureRedirect: "/",
+        failureFlash: true,
+      }),
+      (_req, res) => {
+        res.redirect("/");
+      }
+    );
+
+    // Retrieve SP metadata (Only works in production)
+    const metadataPath = path.resolve(
+      config.get("ServiceProviderMetadataFile")
+    );
+    router.get("/metadata.xml", async (_, res) => {
+      res.sendFile(metadataPath);
+    });
+
+    // Route to logout.
+    router.get("/logout", (req, res) => {
+      req.logout();
       res.redirect("/");
-    }
-  );
-
-  // Retrieve SP metadata (Only works in production)
-  const metadataPath = path.resolve(
-    config.get("ServiceProviderMetadataFile")
-  );
-  router.get("/metadata.xml", async (_, res) => {
-    res.sendFile(metadataPath);
-  });
-
-  // Route to logout.
-  router.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
+    });
+  } else {
+    // Joi inputvalidation
+    const mockUserSchema = Joi.object({
+      netid: Joi.string().required(),
+      affiliation: Joi.string().required(),
+    });
+    // Mock login route
+    router.post(
+      "/mocklogin",
+      validateBody(mockUserSchema),
+      async (req, _res, next) => {
+        const netid = req.body.netid;
+        const affiliation = req.body.affiliation;
+        console.log(`Mocklogin: ${netid}, ${affiliation}`);
+        // make Mocked passport configuration
+        await mockPassportConfiguration(passport, netid, affiliation);
+        next();
+      },
+      passport.authenticate("mock"),
+      (_req, res) => {
+        res.redirect("/");
+      }
+    );
+  }
 };
 
 export default authenticationRoutes;
