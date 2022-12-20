3eb8a1e1be5c3132cf2fd3b57b83a6de238b35f2
Taico Aerts <t.v.aerts@tudelft.nl>
2022-12-20 15:21:45 +0100

    Apply 1 suggestion(s) to 1 file(s)

--- client/src/main.js
+++ client/src/main.js
@@ -64,7 +64,7 @@ Sentry.init({
     // Set tracesSampleRate to 1.0 to capture 100%
     // of transactions for performance monitoring.
     // We recommend adjusting this value in production
-    tracesSampleRate: 1.0
+    tracesSampleRate: process.env.VUE_APP_SENTRY_TRACES_SAMPLE_RATE
 })
 
 new Vue({
