3ddcef918bba3273284e56f2918ceb8078e4a742
Taico Aerts <t.v.aerts@tudelft.nl>
2023-01-12 15:41:39 +0100

    Pretty up deploy script

--- deploy-production.sh
+++ deploy-production.sh
@@ -15,45 +15,50 @@ USER="$(whoami)"
 
 # Setting up the new release
 echo "0 - INIT"
-# Symlink the config
 echo "         Ensuring config dir exists at $RELEASE_DIR/server/config"
-echo "         Symlinking server/config/production.json..."
-# Create folder, delete file from repo (if it exists) and then symlink
 mkdir -p "$RELEASE_DIR/server/config" || exit 2
+
+echo "         Symlinking server/config/production.json..."
 rm "$RELEASE_DIR/server/config/production.json" || true
 ln -s "$SHARED_DIR/server/config/production.json" "$RELEASE_DIR/server/config/production.json" || exit 3
+
 echo "         Symlinking server/sessions..."
 ln -sfn "$SHARED_DIR/server/sessions" "$RELEASE_DIR/server/sessions" || exit 4
+
+echo "         Symlinking client/.env.production..."
+rm "$SHARED_DIR/client/.env.production" || true
+ln -s "$SHARED_DIR/client/.env.production" "$RELEASE_DIR/client/.env.production" || exit 5
 
 # Backup
 echo "1 - BACKUP"
 echo "         Backing up database"
-sudo backupninja -n || exit 5
+sudo backupninja -n || exit 10
 
 # Install packages
 echo "2 - INSTALL"
 echo "         Installing server packages"
 cd "$RELEASE_DIR/server"
-npm install || exit 6
+npm install || exit 20
 echo "         Installing client packages"
 cd "$RELEASE_DIR/client"
-npm install || exit 7
+npm install || exit 21
 
 # Build npm
 echo "3 - BUILD"
 echo "         Building server"
 cd "$RELEASE_DIR"
-NODE_ENV=production && SENTRY_RELEASE="$TIMESTAMP" && npm run build_server || exit 8
+NODE_ENV=production SENTRY_RELEASE="$TIMESTAMP" npm run build_server || exit 30
 
-# Create server release
+echo "         Creating sentry release (server)"
 sentry-cli releases new --project "peer-backend" "$TIMESTAMP"
 sentry-cli releases files --project "peer-backend" "$TIMESTAMP" upload-sourcemaps server/dist
 sentry-cli releases finalize --project "peer-backend" "$TIMESTAMP"
 
 echo "         Building client"
-NODE_ENV=production && echo "VUE_APP_SENTRY_RELEASE=$TIMESTAMP" > "$RELEASE_DIR/client/.env" && npm run build_client || exit 9
+echo "VUE_APP_SENTRY_RELEASE=$TIMESTAMP" > "$RELEASE_DIR/client/.env"
+NODE_ENV=production npm run build_client || exit 31
 
-#Create client release
+echo "         Creating sentry release (client)"
 sentry-cli releases new --project "peer-frontend" "$TIMESTAMP"
 sentry-cli releases files --project "peer-frontend" "$TIMESTAMP" upload-sourcemaps server/dist/public
 sentry-cli releases finalize --project "peer-frontend" "$TIMESTAMP"
@@ -62,17 +67,17 @@ sentry-cli releases finalize --project "peer-frontend" "$TIMESTAMP"
 echo "4 - MIGRATE"
 echo "         Running migrations"
 cd "$RELEASE_DIR/server"
-NODE_ENV=production npm run typeorm:migration:run || exit 10
+NODE_ENV=production npm run typeorm:migration:run || exit 40
 
 # Switch the symlink
 echo "5 - DEPLOY"
 echo "         Switching symlink to new version"
-ln -snf "$RELEASE_DIR" "$DEPLOY_PATH/current" || exit 11
+ln -snf "$RELEASE_DIR" "$DEPLOY_PATH/current" || exit 50
 
 # Cleanup old releases
 echo "6 - CLEANUP"
 echo "         Removing old releases (keeping last 5)"
-cd "$DEPLOY_PATH/releases" || exit 12
+cd "$DEPLOY_PATH/releases" || exit 60
 ls -rp "$DEPLOY_PATH/releases" | grep "/$" | tail -n +6 | xargs -d '\n' -r rm -r --
 
 # Restart the service
--- deploy-staging.sh
+++ deploy-staging.sh
@@ -15,13 +15,20 @@ USER="$(whoami)"
 
 # Setting up the new release
 echo "0 - INIT"
-# Symlink the config
 echo "         Ensuring config dir exists at $RELEASE_DIR/server/config"
-echo "         Symlinking server/config/staging.json..."
-# Create folder, delete file from repo (if it exists) and then symlink
 mkdir -p "$RELEASE_DIR/server/config" || exit 2
+
+echo "         Symlinking server/config/staging.json..."
 rm "$RELEASE_DIR/server/config/staging.json" || true
-ln -s "$SHARED_DIR/config/staging.json" "$RELEASE_DIR/server/config/staging.json" || exit 3
+ln -s "$SHARED_DIR/server/config/staging.json" "$RELEASE_DIR/server/config/staging.json" || exit 3
+
+echo "         Symlinking server/sessions..."
+ln -sfn "$SHARED_DIR/server/sessions" "$RELEASE_DIR/server/sessions" || exit 4
+
+echo "         Symlinking client/.env.staging..."
+rm "$SHARED_DIR/client/.env.staging" || true
+ln -s "$SHARED_DIR/client/.env.staging" "$RELEASE_DIR/client/.env.staging" || exit 5
+
 
 # Backup
 echo "1 - BACKUP"
@@ -31,26 +38,27 @@ echo "         Skipping backup for staging"
 echo "2 - INSTALL"
 echo "         Installing server packages"
 cd "$RELEASE_DIR/server"
-npm install || exit 5
+npm install || exit 20
 echo "         Installing client packages"
 cd "$RELEASE_DIR/client"
-npm install || exit 6
+npm install || exit 21
 
 # Build npm
 echo "3 - BUILD"
 echo "         Building server"
 cd "$RELEASE_DIR"
-NODE_ENV=staging && SENTRY_RELEASE="$TIMESTAMP" && npm run build_server || exit 7
+NODE_ENV=staging SENTRY_RELEASE="staging_$TIMESTAMP" npm run build_server || exit 30
 
-# Create server release
+echo "         Creating sentry release (server)"
 sentry-cli releases new --project "peer-backend" "$TIMESTAMP"
 sentry-cli releases files --project "peer-backend" "$TIMESTAMP" upload-sourcemaps server/dist
 sentry-cli releases finalize --project "peer-backend" "$TIMESTAMP"
 
 echo "         Building client"
-NODE_ENV=staging && echo "VUE_APP_SENTRY_RELEASE=$TIMESTAMP" > "$RELEASE_DIR/client/.env" && npm run build_client || exit 8
+echo "VUE_APP_SENTRY_RELEASE=staging_$TIMESTAMP" > "$RELEASE_DIR/client/.env"
+NODE_ENV=staging npm run build_client || exit 31
 
-#Create client release
+echo "         Creating sentry release (client)"
 sentry-cli releases new --project "peer-frontend" "$TIMESTAMP"
 sentry-cli releases files --project "peer-frontend" "$TIMESTAMP" upload-sourcemaps server/dist/public
 sentry-cli releases finalize --project "peer-frontend" "$TIMESTAMP"
@@ -59,17 +67,17 @@ sentry-cli releases finalize --project "peer-frontend" "$TIMESTAMP"
 echo "4 - MIGRATE"
 echo "         Running migrations"
 cd "$RELEASE_DIR/server"
-NODE_ENV=staging npm run typeorm:migration:run || exit 9
+NODE_ENV=staging npm run typeorm:migration:run || exit 40
 
 # Switch the symlink
 echo "5 - DEPLOY"
 echo "         Switching symlink to new version"
-ln -snf "$RELEASE_DIR" "$DEPLOY_PATH/current" || exit 10
+ln -snf "$RELEASE_DIR" "$DEPLOY_PATH/current" || exit 50
 
 # Cleanup old releases
 echo "6 - CLEANUP"
 echo "         Removing old releases (keeping last 5)"
-cd "$DEPLOY_PATH/releases" || exit 11
+cd "$DEPLOY_PATH/releases" || exit 60
 ls -rp "$DEPLOY_PATH/releases" | grep "/$" | tail -n +6 | xargs -d '\n' -r rm -r --
 
 # Restart the service
