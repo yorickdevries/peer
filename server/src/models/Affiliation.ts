15b6aee0bfb61f61550bed42a947c26d314da723
Philip De Munck <P.L.DeMunck@student.tudelft.nl>
2023-07-17 20:11:07 +0200

    Update affiliation

--- server/src/models/Affiliation.ts
+++ server/src/models/Affiliation.ts
@@ -1,9 +1,14 @@
 import { Entity } from "typeorm";
 import NamedModel from "./NamedModel";
 
+interface AffiliationType {
+  name: string;
+}
 @Entity()
 export default class Affiliation extends NamedModel {
-  constructor(name: string) {
-    super(name);
+  constructor(init?: AffiliationType) {
+    if (init !== undefined) {
+      super(init.name);
+    }
   }
 }
