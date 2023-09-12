00a15d846b570772d31a8fb4b573ac97657327ae
Lohithsai Yadala Chanchu <lyadalachanchu@tudelft.nl>
2023-09-12 21:27:56 +0200

    add jupyter resources + get local server started on client side

--- client/src/components/teacher-dashboard/assignments/CreateAssignment.vue
+++ client/src/components/teacher-dashboard/assignments/CreateAssignment.vue
@@ -5,6 +5,7 @@
             <b-row>
                 <b-col>
                     <h1 class="mt-5">Create a new assignment</h1>
+                    <iframe src="http://127.0.0.1:8000/index.html" width="100%" height="500px"></iframe>
                 </b-col>
             </b-row>
 
